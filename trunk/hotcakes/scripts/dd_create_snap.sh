b18f03878ed0edb20ed2d2e016c2785eb4e8fab9
robert.l.nelson <robert.l.nelson@9f37e8af-d821-0410-9430-6fa8b24559eb>
2006-08-29 16:59:37 +0000

    Updated version of the scripts; dd_replicate uses the correct options for compression. Also, included Alex Polvi's comments on making the scripts a little better.

--- trunk/hotcakes/scripts/dd_create_snap.sh
+++ trunk/hotcakes/scripts/dd_create_snap.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 # import all the sweet variables used throughout the ddsnap scripts
 . ddsnap_env.sh
@@ -13,8 +13,5 @@ cd $DDSNAP_HOME
 
 echo "Creating snapshot id $1"
 ./${CREATE_SNAPSHOT} $SERVER_SOCK_NAME $1
-SERVER_SOCK_NAME=@test
-echo 0 $SIZE_LOGICAL_VOL ddsnap $SNAPSTORE_DEV $ORIGIN_DEV $SERVER_SOCK_NAME $1 | dmsetup create ${SNAPSHOT_VOL_NAME}$1
+echo 0 $SIZE_LOGICAL_VOL ddsnap $SNAPSTORE_DEV $ORIGIN_DEV $AGENT_SOCK_NAME $1 | dmsetup create ${SNAPSHOT_VOL_NAME}$1
 
-
-cd -
