826167fe87ca75c369dc1300334e6c0ae2133ad6
alexpolvi <alexpolvi@9f37e8af-d821-0410-9430-6fa8b24559eb>
2006-08-30 14:29:45 +0000

    fixed logic error in usage checking

--- hotcakes/scripts/dd_replicate.sh
+++ hotcakes/scripts/dd_replicate.sh
@@ -3,7 +3,7 @@
 # import all the sweet variables used throughout the ddsnap scripts
 . ddsnap_env.sh
 
-if [ $# -eq 4  ]
+if [ ! $# -eq 4  ]
 then
    echo "usage: `basename $0` [-d|-r|-t] [-c|-n] <older_snapshot_id> <newer_snapshot_id> "
    exit 1
@@ -24,4 +24,3 @@ echo "Creating snapshot $4 on remote host $REMOTE_HOST"
 ssh $REMOTE_HOST ${DDSNAP_HOME}/${CREATE_SNAPSHOT} $SERVER_SOCK_NAME $4
 
 ssh $REMOTE_HOST "(echo 0 $SIZE_LOGICAL_VOL ddsnap $SNAPSTORE_DEV $ORIGIN_DEV $AGENT_SOCK_NAME $4 | dmsetup create ${SNAPSHOT_VOL_NAME}$4) &> /dev/null < /dev/null"
-
