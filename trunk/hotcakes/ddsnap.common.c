5dbe0c0aef3ae441a72b4501217179d02b20666e
rcombs <rcombs@9f37e8af-d821-0410-9430-6fa8b24559eb>
2006-09-20 23:43:06 +0000

    New files.  They implement common chunk address array handling code used by ddsnap.c and ddsnapd.c.

---
+++ trunk/hotcakes/ddsnap.common.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+#include <unistd.h> /* for ddnsap.h */
+#include "ddsnap.h"
+#include "ddsnap.common.h"
+
+
+#define CHUNK_ARRAY_INIT 1024
+
+
+struct change_list *init_change_list(u32 chunksize_bits)
+{
+	struct change_list *cl;
+
+	if ((cl = malloc(sizeof(struct change_list))) == NULL)
+		return NULL;
+
+	cl->count = 0;
+	cl->length = CHUNK_ARRAY_INIT;
+	cl->chunksize_bits = chunksize_bits;
+	cl->chunks = malloc(cl->length * sizeof(u64));
+
+	if (cl->chunks == NULL) {
+		free(cl);
+		return NULL;
+	}
+
+	return cl;
+}
+
+int append_change_list(struct change_list *cl, u64 chunkaddr)
+{
+	if ((cl->count + 1) >= cl->length) {
+		u64 *newchunks;
+
+		/* we could use a different strategy here like incrementing but this
+		 * should work and will avoid having too many
+		 * reallocations
+		 */
+		if ((newchunks = realloc(cl->chunks, cl->length * 2 * sizeof(u64))) == NULL)
+		    return -1;
+
+		cl->length *= 2;
+		cl->chunks = newchunks;
+	}
+
+	cl->chunks[cl->count] = chunkaddr;
+	cl->count++;
+
+	return 0;
+}
+
+void free_change_list(struct change_list *cl)
+{
+	free(cl->chunks);
+	free(cl);
+}
+
---
+++ trunk/hotcakes/ddsnap.common.h
@@ -0,0 +1,14 @@
+
+struct change_list
+{
+	unsigned count;
+	unsigned length;
+	u32 chunksize_bits;
+	u64 *chunks;
+};
+
+
+extern struct change_list *init_change_list(u32 chunksize_bits);
+extern int append_change_list(struct change_list *cl, u64 chunkaddr);
+extern void free_change_list(struct change_list *cl);
+
