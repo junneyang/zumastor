71f121bae3dad5b80758b625faec585398cc42e2
Daniel.Raymond.Phillips <Daniel.Raymond.Phillips@9f37e8af-d821-0410-9430-6fa8b24559eb>
2007-03-30 00:32:44 +0000

    Get rid of breakpoint interrupt on error exit, exit(99) instead (i.e., this error can't happen)

--- ddsnap/trace.h
+++ ddsnap/trace.h
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <time.h>
 
-#define BREAK asm("int3")
+#define BREAK exit(99);
 #define warn(string, args...) do { time_t _warn_time = time(NULL); char *_warn_ctime=ctime(&_warn_time); if (_warn_ctime[strlen(_warn_ctime)-1] == '\n') _warn_ctime[strlen(_warn_ctime)-1] = '\0'; fprintf(stderr, "%s: [%u] %s: " string "\n", _warn_ctime, getpid(), __func__, ##args); } while (0)
 #define error(string, args...) do { warn(string, ##args); BREAK; } while (0) 
 #define assert(expr) do { if (!(expr)) error("Failed assertion \"%s\"\n", #expr); } while (0)
