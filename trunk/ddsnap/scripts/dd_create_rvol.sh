0e9ab831f1ff366758b6387a51ce331b03b9fadb
Daniel.Raymond.Phillips <Daniel.Raymond.Phillips@9f37e8af-d821-0410-9430-6fa8b24559eb>
2007-04-21 02:18:09 +0000

    Remove Rob's original bash scripts that used to drive ddraid before our zumastor script rose to prominence

--- trunk/ddsnap/scripts/dd_create_rvol.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-# import all the sweet variables used throughout the ddsnap scripts
-. ddsnap_env.sh
-
-cd $DDSNAP_HOME
-
-echo "Creating logical volume remotely"
-ssh $REMOTE_HOST "(echo 0 $SIZE_LOGICAL_VOL ddsnap $SNAPSTORE_DEV $ORIGIN_DEV $AGENT_SOCK_NAME -1 | dmsetup create $LOGICAL_VOL_NAME) &> /dev/null < /dev/null"
