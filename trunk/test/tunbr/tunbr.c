b620ce4e1c36c8e14e68613aee8afc8f88f4a48e
drake.diedrich <drake.diedrich@9f37e8af-d821-0410-9430-6fa8b24559eb>
2007-06-07 23:27:35 +0000

    - Added autogenerated MACADDR and a permissive MACFILE located at the matching pxelinux.cfg location for the mac address. - ifconfig tapX up

--- trunk/test/tunbr/tunbr.c
+++ trunk/test/tunbr/tunbr.c
@@ -23,6 +23,8 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 extern char **environ;
 
@@ -39,15 +41,84 @@ extern char **environ;
 #define BRCTL "/usr/sbin/brctl"
 #endif
 
+#ifndef IFCONFIG
+#define IFCONFIG "/sbin/ifconfig"
+#endif
+
+#ifndef PXELINUXCFGDIR
+#define PXELINUXCFGDIR "/tftpboot/pxelinux.cfg/"
+#endif
+
+
+
+
+
+
+
+
 int main(int argc,char **argv) {
   uid_t uid;
   pid_t child;
   char command[256], device[16];
   int n, tapn, nuid, rv, status;
   FILE *tunctlfp;
+  int rfd, mfd;
+  char macfile[256], macaddr[20];
+  unsigned char mac[6];
   
+
   uid = getuid();
+
   
+  rfd = open("/dev/urandom", O_RDONLY);
+  if (rfd) {
+    n = read(rfd,mac,sizeof(mac));
+    if (n != sizeof(mac)) {
+      perror("short read");
+      return(-7);
+    }
+    close(rfd);
+    mac[0] &= 0xfe;
+
+    n = snprintf(macfile, sizeof(macfile),
+		 PXELINUXCFGDIR "01-%02x-%02x-%02x-%02x-%02x-%02x",
+		 mac[0], mac[1], mac[2], mac[3], mac[4], mac[5], mac[6]);
+    mfd = creat(macfile,0644);
+    if (mfd == -1) {
+      perror("create macfile");
+      return errno;
+    }
+    rv = close(mfd);
+    if (rv == -1) {
+      perror("close macfile fd");
+      return errno;
+    }
+    rv = chown(macfile, uid, -1);
+    if (rv == -1) {
+      perror("chown");
+      return errno;
+    }
+    rv = chmod(macfile, 0644);
+    if (rv == -1) {
+      perror("chmod");
+      return errno;
+    }
+    rv = setenv("MACFILE", macfile);
+    if (rv == -1) {
+      fprintf(stderr, "insufficient environment space for MACFILE variable\n");
+      return -12;
+    }
+
+    n = snprintf(macaddr, sizeof(macaddr),
+		 "%02x:%02x:%02x:%02x:%02x:%02x",
+		 mac[0], mac[1], mac[2], mac[3], mac[4], mac[5], mac[6]);
+    rv = setenv("MACADDR", macaddr);
+    if (rv == -1) {
+      fprintf(stderr, "insufficient environment space for MACADDR variable\n");
+      return -11;
+    }
+  }
+
 
   n = snprintf(command, sizeof(command), TUNCTL " -u %d", uid);
 
@@ -58,7 +129,6 @@ int main(int argc,char **argv) {
     return errno;
   }
 
-
   n = fscanf(tunctlfp, "Set 'tap%d' persistent and owned by uid %d",
              &tapn, &nuid);
   if (n != 2) {
@@ -66,7 +136,6 @@ int main(int argc,char **argv) {
 	    command);
     return -1;
   }
-
 
   if (pclose(tunctlfp) == -1) {
     perror("tunctlfp close failed");
@@ -86,13 +155,26 @@ int main(int argc,char **argv) {
     return -2;
   }
 
-  n = snprintf(device, sizeof(device), "tap%d", tapn);
 
+  n = snprintf(command, sizeof(command), IFCONFIG " tap%d up", tapn);
+  rv = system(command);
+  if (rv == -1) {
+    perror(TUNCTL);
+    return errno;
+  }
+  if (rv) {
+    fprintf(stderr, "'%s' returned %d", command, rv);
+    return -10;
+  }
+
+
+  n = snprintf(device, sizeof(device), "tap%d", tapn);
   rv = setenv("IFACE", device);
   if (rv == -1) {
     fprintf(stderr, "insufficient environment space for IFACE variable\n");
     return -5;
   }
+
 
   child = fork();
   if (child>0) {
@@ -129,6 +211,14 @@ int main(int argc,char **argv) {
     fprintf(stderr, "'%s' returned %d\n", command, rv);
     return -4;
   }
+
+
+  rv = unlink(macfile);
+  if (rv == -1) {
+    perror("unlink");
+    return errno;
+  }
+
 
   return 0;
 }
