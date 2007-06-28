ad1308d2c79209394a2a58c057df400d1dda83bf
drake.diedrich <drake.diedrich@9f37e8af-d821-0410-9430-6fa8b24559eb>
2007-06-28 21:39:22 +0000

    Test image template setups.

---
+++ trunk/test/scripts/etch-late.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# run after the base packages are installed, from inside the d-i environment
+# copied into the initrd so it doesn't need to be fetched over network
+# connections.
+
+# $Id$
+# Copyright 2007 Google Inc.
+# Author: Drake Diedrich <dld@google.com>
+# License: GPLv2
+
+set -e
+
+mkdir /target/root/.ssh
+cp /authorized_keys /target/root/.ssh
+
+echo 'Acquire::http::Pipeline-Depth "0";' > /target/etc/apt/apt.conf.d/01cache
+
+apt-install install openssh-server
+  
