658764ee300f3e1dd31420795ab3c310483b55eb
fmayhar <fmayhar@9f37e8af-d821-0410-9430-6fa8b24559eb>
2007-07-05 18:23:20 +0000

    Attempt to fix the IP address/hostname configuration problems.  Create a new ethernet bridge and hang the host interfaces off that; IPs are in a non-routable range and are allocated sequentially based on the instance number.  Currently 192.168.22.20+.

--- test/scripts/qemuinstall.sh
+++ test/scripts/qemuinstall.sh
@@ -22,6 +22,12 @@ DISKIMG=${WORKDIR}/qimage${IMGSUFFIX}
 # Prefix of the name of the live image.  The image number is appended to this
 # along with the suffix ".img", e.g. "qlive-1.img".
 QLIVEPREFIX=qlive-
+# IP network /24 prefix for virtual machine addresses.
+IPNET=192.168.22
+IPSTART=20
+# Fake "ethernet" MAC address prefix.
+MAC=00:e0:10:00:00
+MACSTART=0
 # Prefix of the volume to be used for Zumastor testing.
 ZUMVOLUME=${WORKDIR}/zumavol-
 # Name of the install directory.  This must match the name in the preseed file!
@@ -35,11 +41,6 @@ QRUNDIR=/etc/rc2.d
 #
 PACKAGEDEPS="tree dmsetup openssh-server make gcc libc6-dev"
 
-#
-# Where we can find the Zumastor package files.
-#
-PACKAGEDIR=""
-
 canonicalize_hostname()
 {
 	oname=$1
@@ -50,27 +51,31 @@ canonicalize_hostname()
 	fi
 	case "$2" in
 	has)	cname=$1;;
-	domain)	cname=$5;;
+	domain)	cname=$5
+		set `host ${cname} | sed -e 's/\.$//'`
+		if [ "$3" = "not" -a "$4" = "found:" ]; then
+			echo ${oname}
+		fi
+		;;
 	*)	cname=$oname;;
 	esac
 	echo $cname
 }
 
+
 usage()
 {
-	echo "Usage: $0 [-p <path>] [-i <image>] [-n <number of images>" 1>&2
+	echo "Usage: $0 [-i <image>] [-n <number of images>" 1>&2
 	echo "Where <path> is a directory that contains the Zumastor .deb files,"
 	echo "<image> is the CD image from which to install the virtual images and"
 	echo "<number of images> is the number of images to create, by default one."
 	exit 1
 }
 
-PACKAGEDIR=""
-while getopts "i:n:p:" option ; do
+while getopts "i:n:" option ; do
 	case "$option" in
 	i)	CDIMAGE=${OPTARG};;
 	n)	NUMIMAGES=${OPTARG};;
-	p)	PACKAGEDIR="${OPTARG}";;
 	*)	usage;;
 	esac
 done
@@ -87,16 +92,6 @@ echo "Building ${NUMIMAGES} qemu image(s)..."
 if [ "${CDIMAGE:0:1}" != "/" ]; then
 	CDIMAGE=`pwd`/${CDIMAGE}
 fi
-#
-# If the package directory doesn't start with a "/" then it's also relative
-# to the current directory.
-#
-if [ "${PACKAGEDIR}" != "" ]; then
-	if [ "${PACKAGEDIR:0:1}" != "/" ]; then
-		PACKAGEDIR=`pwd`/${PACKAGEDIR}
-	fi
-	PACKAGEDIR="-p ${PACKAGEDIR}"
-fi
 
 cd ${WORKDIR}
 #
@@ -105,11 +100,21 @@ cd ${WORKDIR}
 cat <<EOF_qemu-ifup.sh >qemu-ifup.sh
 #!/bin/sh
 
+set -x
+
+#
+# If the qemu bridge doesn't exist, create it.
+#
+brctl show | grep qbr0 >/dev/null 2>&1
+if [ \$? -ne 0 ]; then
+	brctl addbr qbr0
+	ifconfig qbr0 \$HOSTIP
+fi
 echo "Executing \$0"
-echo "Bringing up \$1 for bridged mode..."
+echo "Bringing up \$1 \$QIP for bridged mode..."
 sudo /sbin/ifconfig \$1 0.0.0.0 promisc up
-echo "Adding \$1 to br0..."
-sudo /usr/sbin/brctl addif br0 \$1
+echo "Adding \$1 to qbr0..."
+sudo /usr/sbin/brctl addif qbr0 \$1
 EOF_qemu-ifup.sh
 chmod 755 qemu-ifup.sh
 #
@@ -117,8 +122,6 @@ chmod 755 qemu-ifup.sh
 #
 cat <<EOF_zinstall.sh >zinstall.sh
 #!/bin/sh
-
-set -x
 
 cd /${QINSTDIR}
 #
@@ -140,14 +143,10 @@ cat hostkey.pub >>/root/.ssh/authorized_keys
 # Set 'noapic' on the kernel(s) since qemu doesn't have one.
 #
 sed --in-place '/^kernel/s/$/ noapic/' /boot/grub/menu.lst >/dev/console 2>&1
-#
-# Add extra interfaces to /etc/network/interfaces
-#
-cat <<EOF_network >>/etc/network/interfaces
-auto eth1
-iface eth1 inet dhcp
-auto eth2
-iface eth2 inet dhcp
+
+#cat <<EOF_inittab >>/etc/inittab
+#T0:23:respawn:/sbin/getty -L -l /bin/sh ttyS0 9600 vt100
+#EOF_inittab
 
 exit 0
 EOF_zinstall.sh
@@ -162,12 +161,21 @@ cat <<EOF_zstartup >zstartup.sh
 #
 # All we do here is send our configured IP address out our console.
 #
-echo "**********************************" >>/dev/ttyS0
-echo "**********************************" >>/dev/ttyS0
-ifconfig eth1 | grep "inet addr:" >>/dev/ttyS0
-echo "**********************************" >>/dev/ttyS0
-echo "**********************************" >>/dev/ttyS0
-echo "**********************************" >>/dev/ttyS0
+#echo "**********************************" >>/dev/ttyS0
+#echo "**********************************" >>/dev/ttyS0
+ifconfig -a | grep eth >>/dev/ttyS0
+sleep 1
+ifconfig -a | grep eth >>/dev/ttyS0
+sleep 1
+ifconfig -a | grep eth >>/dev/ttyS0
+sleep 1
+ifconfig -a | grep eth >>/dev/ttyS0
+#echo "**********************************" >>/dev/ttyS0
+#echo "**********************************" >>/dev/ttyS0
+#echo "**********************************" >>/dev/ttyS0
+
+# Crank up a shell
+/bin/sh -l </dev/ttyS0 >/dev/console 2>&1 &
 
 # Remove the startup script now that it has fulfilled its purpose.
 rm /etc/init.d/zstartup
@@ -179,12 +187,44 @@ rm ${QRUNDIR}/S99zstartup
 exit 0
 EOF_zstartup
 chmod 755 zstartup.sh
+#
+# Create the network-configuration script.  This script is run shortly
+# after boot via the serial console connection; it receives the interface
+# to configure, IP address and hostname of the virtual machine.
+#
+cat <<EOF_znet >znet
+#/bin/sh
+
+echo \$1 \$2 \$3
+ifconfig \$1 inet \$2 netmask 255.255.255.0
+hostname \$3
+#
+# Add extra interfaces to /etc/network/interfaces
+#
+cat <<EOF_network >>/etc/network/interfaces
+auto \$1
+iface \$1 inet static
+	address \$2
+	netmask 255.255.255.0
+#auto eth2
+#iface eth2 inet static
+#	address 0.0.0.0
+EOF_network
+
+echo \$3 >/etc/hostname
+
+ifdown eth1
+ifup eth1
+
+EOF_znet
+chmod 755 znet
+
 zinstar=${WORKDIR}/zinstar-$$.tar
 #
 # If we've already built the master image, don't do it again.
 #
 if [ ! -f ${DISKIMG} ]; then
-	trap "rm -f ${CDIMAGE} ${zinstar} ${DISKIMG}; exit 1" 1 2 3 9
+	trap "rm -f ${zinstar} ${DISKIMG}; exit 1" 1 2 3 9
 	echo "Building master..."
 	#
 	# Generate (if necessary) and grab our ssh key to hand to the virtual
@@ -197,7 +237,7 @@ if [ ! -f ${DISKIMG} ]; then
 	#
 	# Stuff scripts and keys into a tarfile.
 	#
-	tar cf ${zinstar} zinstall.sh zstartup.sh hostkey.pub
+	tar cf ${zinstar} zinstall.sh zstartup.sh znet hostkey.pub
 	#
 	# Create the qemu disk image then boot qemu from our iso to do the
 	# install.
@@ -206,10 +246,13 @@ if [ ! -f ${DISKIMG} ]; then
 	qemu -no-reboot -hdb ${zinstar} -cdrom ${CDIMAGE} -boot d ${DISKIMG}
 
 	rm ${zinstar}
+	trap - 1 2 3 9
 fi
 
-trap - 1 2 3 9
 
+# Export the IP address variable.
+export QIP QMAC QIF
+export HOSTIP=${IPNET}.${IPSTART}
 #
 # Now clone the base image, boot the new image and collect its IP address.
 #
@@ -219,41 +262,50 @@ while [ ${imagenum} -le ${NUMIMAGES} ]; do
 	# Clone the test disk image for our live instance.
 	thisimg=${WORKDIR}/${QLIVEPREFIX}${imagenum}${IMGSUFFIX}
 	thisname=${QLIVEPREFIX}${imagenum}
+	qconsole=qemu-${imagenum}.cons
 	cp ${DISKIMG} ${thisimg} &
 	rm -f qemu-${imagenum}.pid qemu-${imagenum}.cons
 	mkfifo qemu-${imagenum}.cons
 	# Create the Zumastor test volume.
 	zvol=${ZUMVOLUME}${imagenum}${IMGSUFFIX}
 	qemu-img create -f qcow2 ${zvol} 32G
+	# Compute new IP/MAC addresses, interface number.
+	QIP=${IPNET}.$((${IPSTART} + ${imagenum}))
+	QMAC=${MAC}:`printf "%2.2x" $((${MACSTART} + ${imagenum}))`
+	QIF=eth${imagenum}
 	# Wait for the test disk image copy to complete.
 	wait
 	#
-	# Run qemu once to do the post-boot configuration and so we can get
-	# the IP address of the virtual machine.  This one will try to reboot;
-	# the "no-reboot" option means that it just shuts down.
+	# Run qemu on the cloned image.  We configure the network and hostname
+	# then leave the instance running.
 	#
 	echo '********** WARNING:  Running sudo.  You may have to type your password!'
-	sudo qemu --pidfile qemu-${imagenum}.pid -serial pipe:qemu-${imagenum}.cons -m 512 -hdb ${zvol} -cdrom ${CDIMAGE} -boot c -net nic,macaddr=00:e0:10:00:00:0${imagenum} -net tap,ifname=eth${imagenum},script=${WORKDIR}/qemu-ifup.sh ${thisimg} &
-	QIP=
-	while [ -z "${QIP}" ]; do
-		QIP=`dd if=qemu-${imagenum}.cons bs=80 count=2 | grep "inet addr:" | tail -1 | sed -e "s/	/ /g" | sed -e 's/^.*addr:\(.*\).*Bcast:.*$/\1/'`
+	sudo qemu --pidfile qemu-${imagenum}.pid -serial pipe:${qconsole} -m 512 -hdb ${zvol} \
+		-cdrom ${CDIMAGE} -boot c \
+		-net nic,macaddr=${QMAC} -net tap,ifname=${QIF},script=${WORKDIR}/qemu-ifup.sh \
+		${thisimg} &
+	IFACE=
+	while [ -z "${IFACE}" ]; do
+		IFACE=`dd if=${qconsole} bs=80 count=2 | grep eth | cut -f1 -d' '`
 	done
-	echo "${QIP}	${thisname}"
+	echo ${IFACE}
 	sleep 5
-	#sh -x ${ZUMINSTALL} ${PACKAGEDIR} ${QIP}
-#	wait
-#	rm -f qemu-${imagenum}.pid
 	#
-	# Now run qemu for the last time, this time in the background.  We
-	# leave this instance running after we ssh in and set its hostname.
+	# Try to configure the interface.
 	#
-#	echo '********** WARNING:  Running sudo (again).  You may have to type your password!'
-#	sudo qemu --pidfile qemu-${imagenum}.pid -serial pipe:qemu-${imagenum}.cons -m 512 -hdb ${zvol} -boot c -net nic,macaddr=00:e0:10:00:00:0${imagenum} -net tap,ifname=eth${imagenum},script=${WORKDIR}/qemu-ifup.sh ${thisimg} &
-#	sleep 60
+	echo -ne "\r\r" >>${qconsole}
+	echo -n "${QINSTDIR}/znet " >>${qconsole}
+	sleep 1
+	echo -n "${IFACE} ${QIP} ${thisname}" >>${qconsole}
+	sleep 1
+	echo -ne "\r" >>${qconsole}
+	#
+	# Disown the currently-running qemu; he's now on his own.
+	#
 	disown -a
 	grep -v ${QIP} ~/.ssh/known_hosts >/tmp/kn.$$
-	cat /tmp/kn.$$ >~/.ssh/known_hosts
-	rm /tmp/kn.$$
+	mv /tmp/kn.$$ ~/.ssh/known_hosts
+	sleep 3
 	ssh-keyscan ${QIP}
 	hostname=`canonicalize_hostname ${QIP}`
 	if [ "${hostname}" = "" ]; then
