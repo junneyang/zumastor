6fd357c4c4455348941dec74305fcce2dc491521
drake.diedrich <drake.diedrich@9f37e8af-d821-0410-9430-6fa8b24559eb>
2007-07-13 20:59:30 +0000

    Separate continuous build into the bare loop and a separate (updatable) runtests.sh script to fire off the actual tests.

--- continuous.sh
+++ continuous.sh
@@ -2,29 +2,12 @@
 #
 # $Id$
 #
-# Continously svn update zumastor, build packages, install them, and run the
-# tests.
+# Continuously svn update and run the test suite.
 
 set -e
-
-diskimgdir=${HOME}/.testenv
-
-[ -x /etc/default/testenv ] && . /etc/default/testenv
-
 
 while true
 do
   svn update
-  REVISION=`svn info | awk '/Revision:/ { print $2; }'`
-  export REVISION
-  rm -f *.deb
-  rm -rf build
-  time ./build_packages.sh test/config/qemu-config
-  rm -f ${diskimgdir}/zuma/dapper-i386.img
-  (cd test/scripts ; tunbr ./zuma-dapper-i386.sh ; \
-    if time tunbr ./test-zuma-dapper-i386.sh snapshot-test.sh ; then \
-      echo -n "Revision $REVISION is good " >> build.log ; else \
-      echo -n "Revision $REVISION is bad " >> build.log ; fi ; \
-    date >> build.log
-  )
+  ./runtests.sh
 done
---
+++ runtests.sh
@@ -0,0 +1,32 @@
+#!/bin/sh -x
+#
+# $Id$
+#
+# Run the full test cycle once.  May be updated, changing the behavior
+# of the continuous build between versions.
+#
+# Copyright 2007 Google Inc.  All rights reserved.
+# Author: Drake Diedrich (dld@google.com)
+# License: GPLv2
+#
+
+set -e
+
+diskimgdir=${HOME}/.testenv
+[ -x /etc/default/testenv ] && . /etc/default/testenv
+
+REVISION=`svn info | awk '/Revision:/ { print $2; }'`
+export REVISION
+rm -rf build
+time ./build_packages.sh test/config/qemu-config
+rm -f ${diskimgdir}/zuma/dapper-i386.img
+
+cd test/scripts
+tunbr ./zuma-dapper-i386.sh
+if time tunbr ./test-zuma-dapper-i386.sh snapshot-test.sh
+then
+  echo -n "Revision $REVISION is good " >> build.log
+else
+  echo -n "Revision $REVISION is bad " >> build.log
+fi
+date >> build.log
