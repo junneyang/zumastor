64309e68c4ab9f0b4f8d8431a3713d9731898f22
drake.diedrich <drake.diedrich@9f37e8af-d821-0410-9430-6fa8b24559eb>
2007-07-31 19:48:46 +0000

    Loop checking for revision changes every 5 minutes, then launch the dapper-build and runtests scripts.

--- continuous.sh
+++ continuous.sh
@@ -2,12 +2,33 @@
 #
 # $Id$
 #
-# Continuously svn update and run the test suite.
 
-set -e
+# Continuously svn update, run the encapsulated build, and the test
+# suite.  Copy this and the build-dapper.sh and runtests.sh scripts to
+# the parent directory to avoid running unsafe code on your host
+# machine.  Code direct from the repository is only run on virtual instances.
+
+if [ ! -f zumastor/Changelog ] ; then
+  echo "cp $0 to the parent directory of the zumastor repository and "
+  echo "run from that location.  Periodically inspect the three "
+  echo "host scripts for changes and redeploy them."
+  exit 1
+fi
+
+cd zumastor
+
+oldrevision=""
 
 while true
 do
   svn update
-  ./runtests.sh
+  revision=`svn info | awk '/Revision:/ { print $2; }'`
+  if [ "$revision" = "$oldrevision" ]
+  then
+    sleep 300
+  else
+    ../dapper-build.sh
+    ../runtests.sh
+  fi
+  oldrevision=$revision
 done
