602fe497e33293e57f780007adc3c8eb465c915e
drake.diedrich <drake.diedrich@9f37e8af-d821-0410-9430-6fa8b24559eb>
2007-08-15 18:53:44 +0000

    Reorganize and document continuous build and test environment. Everything run automatically on the host goes into test/continuous/, and is copied out on deployment. The rest of the testenv and once-off image setup code goes into test/testenv/ dapper-i386.sh: don't try to sed .ssh/known_hosts if it doesn't exist. continuous.sh: test failures go to ${email_failure}, skip successful build logs

---
+++ test/testenv/test-dapper-i386.sh
@@ -0,0 +1,79 @@
+#!/bin/sh -x
+
+# Run the dapper/i386 image using -snapshot to verify that it works.
+# The template should be left unmodified.
+# Any parameters are copied to the destination instance and executed as root
+
+# $Id$
+# Copyright 2007 Google Inc.
+# Author: Drake Diedrich <dld@google.com>
+# License: GPLv2
+
+set -e
+
+execfiles="$*"
+
+if [ "x$MACFILE" = "x" -o "x$MACADDR" = "x" -o "x$IFACE" = "x" \
+     -o "x$IPADDR" = "x" ] ; then
+  echo "Run this script under tunbr"
+  exit 1
+fi
+
+# defaults, overridden by /etc/default/testenv if it exists
+# diskimgdir should be local for reasonable performance
+diskimgdir=${HOME}/testenv
+tftpdir=/tftpboot
+qemu_i386=qemu  # could be kvm, kqemu version, etc.  Must be 0.9.0 to net boot.
+
+[ -x /etc/default/testenv ] && . /etc/default/testenv
+
+IMAGE=dapper-i386
+IMAGEDIR=${diskimgdir}/${IMAGE}
+diskimg=${IMAGEDIR}/hda.img
+
+tmpdir=`mktemp -d /tmp/${IMAGE}.XXXXXX`
+SERIAL=${tmpdir}/serial
+MONITOR=${tmpdir}/monitor
+
+
+
+
+if [ ! -f ${diskimg} ] ; then
+
+  echo "No template image ${diskimg} exists yet."
+  echo "Run tunbr dapper-i386.sh first."
+  exit 1
+fi
+
+echo IPADDR=${IPADDR}
+
+${qemu_i386} -snapshot \
+  -net nic,macaddr=${MACADDR},model=ne2k_pci \
+  -net tap,ifname=${IFACE},script=no \
+  -boot c -hda ${diskimg} -no-reboot &
+  
+while ! ssh -o StrictHostKeyChecking=no root@${IPADDR} hostname 2>/dev/null
+do
+  echo -n .
+  sleep 10
+done
+
+date
+
+# execute any parameters here
+if [ "x${execfiles}" != "x" ]
+then
+  scp ${execfiles} root@${IPADDR}: </dev/null || true
+  for f in ${execfiles}
+  do
+    ssh root@${IPADDR} ./${f} || true
+  done
+fi
+
+
+ssh root@${IPADDR} halt
+
+wait
+
+echo "Instance shut down, removing ssh hostkey"
+sed -i /^${IPADDR}\ .*\$/d ~/.ssh/known_hosts
