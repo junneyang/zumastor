675a9df0b09e2da44233136d81f54276dcb775bc
drake.diedrich <drake.diedrich@9f37e8af-d821-0410-9430-6fa8b24559eb>
2007-08-15 18:53:44 +0000

    Reorganize and document continuous build and test environment. Everything run automatically on the host goes into test/continuous/, and is copied out on deployment. The rest of the testenv and once-off image setup code goes into test/testenv/ dapper-i386.sh: don't try to sed .ssh/known_hosts if it doesn't exist. continuous.sh: test failures go to ${email_failure}, skip successful build logs

---
+++ trunk/test/testenv/dapper-i386.sh
@@ -0,0 +1,130 @@
+#!/bin/sh -x
+
+# Set up the initial Ubuntu/dapper template image, for use when duplicating
+# the install to multiple server/client tests.  Makes use of tunbr and
+# a presetup br1, squid, and dnsmasq.
+
+# $Id$
+# Copyright 2007 Google Inc.
+# Author: Drake Diedrich <dld@google.com>
+# License: GPLv2
+
+set -e
+
+SSH='ssh -o StrictHostKeyChecking=no'
+SCP='scp -o StrictHostKeyChecking=no'
+
+if [ "x$MACFILE" = "x" -o "x$MACADDR" = "x" -o "x$IFACE" = "x" \
+     -o "x$IPADDR" = "x" ] ; then
+  echo "Run this script under tunbr"
+  exit 1
+fi
+
+# Remove the any existing ssh hostkey for this IPADDR since generating a
+# new one
+if [ -f ~/.ssh/known_hosts ] ; then
+  sed -i /^${IPADDR}\ .*\$/d ~/.ssh/known_hosts
+fi
+
+# defaults, overridden by /etc/default/testenv if it exists
+# diskimgdir should be local for reasonable performance
+size=10G
+diskimgdir=${HOME}/testenv
+tftpdir=/tftpboot
+qemu_img=qemu-img  # could be kvm, kqemu version, etc.
+qemu_i386=qemu  # could be kvm, kqemu version, etc.
+rqemu_i386=qemu  # could be kvm, kqemu version, etc.  Must be 0.9.0 to net boot.
+VIRTHOST=192.168.23.1
+[ -x /etc/default/testenv ] && . /etc/default/testenv
+
+IMAGE=dapper-i386
+IMAGEDIR=${diskimgdir}/${IMAGE}
+SERIAL=${IMAGEDIR}/serial
+MONITOR=${IMAGEDIR}/monitor
+
+if [ ! -e ${IMAGEDIR} ]; then
+  mkdir -p ${IMAGEDIR}
+  chmod 700 ${IMAGEDIR}
+fi
+
+diskimg=${IMAGEDIR}/hda.img
+
+if [ ! -f ${diskimg} ] ; then
+
+  # extract and repack the initrd with the desired preseed file
+  tmpdir=`mktemp -d`
+  mkdir ${tmpdir}/initrd
+  cp dapper.cfg ${tmpdir}/initrd/preseed.cfg
+  cp common.cfg ${tmpdir}/initrd/
+  cp dapper-early.sh ${tmpdir}/initrd/early.sh
+  cp dapper-late.sh ${tmpdir}/initrd/late.sh
+  passwd=`pwgen 8 1`
+  touch ${IMAGEDIR}/root
+  chmod 600 ${IMAGEDIR}/root
+  echo $passwd > ${IMAGEDIR}/root
+  pwhash=`echo ${passwd} | mkpasswd -s --hash=md5`
+  cat >>${tmpdir}/initrd/preseed.cfg <<EOF
+d-i     mirror/http/hostname    string ${VIRTHOST}
+d-i     passwd/root-password-crypted    password ${pwhash}
+d-i     passwd/user-password-crypted    password ${pwhash}
+d-i	passwd/user-fullname            string ${USER}
+d-i	passwd/username                 string ${USER}
+EOF
+
+  cat ~/.ssh/*.pub > ${tmpdir}/initrd/authorized_keys
+  
+  fakeroot <<EOF
+cd ${tmpdir}/initrd
+zcat ${tftpdir}/ubuntu-installer/i386/initrd.gz | cpio -i
+find . -print0 | cpio -0 -o -H newc | gzip -9 > ${tftpdir}/${USER}/ubuntu-installer/i386/initrd.gz
+EOF
+  chmod ugo+r ${tftpdir}/${USER}/ubuntu-installer/i386/initrd.gz
+  
+  ${qemu_img} create -f qcow2 ${diskimg} ${size}
+
+  cat >${MACFILE} <<EOF
+SERIAL 0 115200 0
+DEFAULT server
+LABEL server
+	kernel ubuntu-installer/i386/linux
+	append base-config/package-selection= base-config/install-language-support=false vga=normal initrd=${USER}/ubuntu-installer/i386/initrd.gz ramdisk_size=13531 root=/dev/rd/0 rw preseed/file=/preseed.cfg DEBCONF_DEBUG=5 console=tty0 console=ttyS0,115200n8
+PROMPT 0
+TIMEOUT 1
+EOF
+  chmod ugo+r ${MACFILE}
+
+  ${rqemu_i386} \
+    -serial unix:${SERIAL},server,nowait \
+    -monitor unix:${MONITOR},server,nowait \
+    -net nic,macaddr=${MACADDR} -net tap,ifname=${IFACE},script=no \
+    -boot n -hda ${diskimg} -no-reboot
+    
+  ${qemu_i386} \
+    -serial unix:${SERIAL},server,nowait \
+    -monitor unix:${MONITOR},server,nowait \
+    -net nic,macaddr=${MACADDR} -net tap,ifname=${IFACE},script=no \
+    -boot c -hda ${diskimg} -no-reboot &
+
+  while ! ${SSH} root@${IPADDR} hostname 2>/dev/null
+  do
+    echo -n .
+    sleep 10
+  done
+
+  # turn the swap partition into LVM2 sysvg
+  ${SCP} swap2sysvg.sh root@${IPADDR}:
+  ${SSH} root@${IPADDR} './swap2sysvg.sh && rm swap2sysvg.sh'
+
+  ${SSH} root@${IPADDR} halt
+
+  wait
+
+  echo "${diskimg} installed.  root and ${USER} passwords are: ${passwd}"
+
+  rm -rf ${tmpdir}
+
+else
+  echo "image ${diskimg} already exists."
+  echo "rm if you wish to recreate it and all of its derivatives."
+fi
+
---
+++ trunk/test/testenv/dapper-late.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# run after the base packages are installed, from inside the d-i environment
+# copied into the initrd so it doesn't need to be fetched over network
+# connections.
+
+# $Id$
+# Copyright 2007 Google Inc.
+# Author: Drake Diedrich <dld@google.com>
+# License: GPLv2
+
+set -e
+
+mkdir /target/root/.ssh
+cp /authorized_keys /target/root/.ssh
+
+apt-install openssh-server cron postfix dmsetup build-essential lvm2
+
+in-target apt-get dist-upgrade -y
+
+in-target apt-get clean
+
+# Since the MAC will change on subsequent copies, get rid of persistence
+rm /target/etc/iftab
+
+# set noapic on all grub kernel boot stanzas
+in-target sed --in-place '/^#kopt=/s/$/ noapic/' /boot/grub/menu.lst
+in-target update-grub
