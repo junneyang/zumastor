da8b2e770f86ddfbd749f6ae8f8665fdeb596544
jiahotcake <jiahotcake@9f37e8af-d821-0410-9430-6fa8b24559eb>
2007-08-22 23:10:53 +0000

    auto test to show potential deadlock on downstream

--- test/uml/test_fullreplication.sh
+++ test/uml/test_fullreplication.sh
@@ -14,14 +14,24 @@ ssh $target_uml_host "zumastor define source $vol $source_uml_host -p $interval"
 echo -e "done.\n"
 sleep 30
 
+# test program that keeps allocating/de-allocating memory
+gcc -o cyclic-anon cyclic-anon.c
+scp cyclic-anon $target_uml_host:/root/cyclic-anon
+ssh $target_uml_host "/root/cyclic-anon 64M" &
+
+# rasing dirty_ratio to trigger problem faster
+ssh $target_uml_host "echo 50 > /proc/sys/vm/dirty_background_ratio" || { echo FAIL; exit 1; }
+ssh $target_uml_host "echo 95 > /proc/sys/vm/dirty_ratio" || { echo FAIL; exit 1; }
+
 echo Monitoring replication progress...
 count=0
 TOTAL=10000
 while [[ $count -lt $TOTAL ]]; do
 	echo -n "source send  "
-	ssh $source_uml_host "cat $VOLUMES/$vol/targets/$target_uml_host/send" || { echo FAIL; exit 1; }
+	ssh $source_uml_host "cat $VOLUMES/$vol/targets/$target_uml_host/send"
+	ssh $target_uml_host "echo" > /dev/null || { echo "downstream not responding, lockup?"; echo FAIL; exit 1; }
 	echo -n "target apply "
-	ssh $target_uml_host "cat $VOLUMES/$vol/source/apply" || { echo FAIL; exit 1; }
+	ssh $target_uml_host "cat $VOLUMES/$vol/source/apply"
 	sleep 5
 	count=$(( count+1 ))
 done
