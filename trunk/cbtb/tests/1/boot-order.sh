343529bfa1f52259575cd230c9d6a5d29281d2ee
drake.diedrich <drake.diedrich@9f37e8af-d821-0410-9430-6fa8b24559eb>
2007-09-06 00:27:45 +0000

    Add LSB dependency comments to the zumastor init.d script boot-order.sh: test installing insserv and asking it whether zumastor's   installed boot order generates a warning from insserv, a dependency-based   boot ordering tool. boot-order.sh is simpler than the other system tests, replacing the built-in    timelimit with a declaration that the host scripts may eventually parse    and use with timeout.

---
+++ trunk/cbtb/tests/1/boot-order.sh
@@ -0,0 +1,29 @@
+#!/bin/sh -x
+#
+# $Id$
+#
+# Boot and install insserv, and make sure zumastor doesn't show up
+# in its list of warnings.
+#
+# Copyright 2007 Google Inc.  All rights reserved
+# Author: Drake Diedrich (dld@google.com)
+
+
+set -e
+
+TIMEOUT=300
+
+echo "1..2"
+rc=0
+
+aptitude install -y insserv nfs-kernel-server
+echo "ok 1 - insserv installed"
+
+if /usr/share/insserv/check-initd-order 2>&1 | egrep zumastor
+then
+  rc=2
+  echo "not ok 2 - zumastor not reported by check-initd-order"
+else
+  echo "ok 2 - zumastor not reported by check-initd-order"
+fi
+exit $rc
