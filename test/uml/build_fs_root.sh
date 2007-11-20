ef404f2f01bc2f3c935ca20c25736a70495ef15a
jiahotcake <jiahotcake@9f37e8af-d821-0410-9430-6fa8b24559eb>
2007-11-20 03:52:40 +0000

    Build ddsnap and zumastor debian packages from repository and install them on uml, instead of downloading them from zumastor.org. Also modify several sentences in README.

--- test/uml/build_fs_root.sh
+++ test/uml/build_fs_root.sh
@@ -18,19 +18,11 @@ chroot /mnt apt-get -q update
 echo -e "done.\n"
 
 echo -n Installing ssh...
-chroot /mnt dpkg -s openssh-client >& /dev/null || chroot /mnt apt-get -y -q install openssh-client || { umount /mnt; exit 1; }
-chroot /mnt dpkg -s openssh-server >& /dev/null || chroot /mnt apt-get -y -q install openssh-server || { umount /mnt; exit 1; }
+chroot /mnt dpkg -s openssh-client >& /dev/null || chroot /mnt apt-get -y -q install openssh-client >& /dev/null || { umount /mnt; exit 1; }
+chroot /mnt dpkg -s openssh-server >& /dev/null || chroot /mnt apt-get -y -q install openssh-server >& /dev/null || { umount /mnt; exit 1; }
 # to get rid of the need for initial confirmation
 grep "StrictHostKeyChecking no" /mnt/etc/ssh/ssh_config >& /dev/null || echo "    StrictHostKeyChecking no" >> /mnt/etc/ssh/ssh_config
 grep "VerifyHostKeyDNS yes" /mnt/etc/ssh/ssh_config >& /dev/null || echo "    VerifyHostKeyDNS yes" >> /mnt/etc/ssh/ssh_config
-echo -e "done.\n"
-
-# required because the debian version of zlib1g does not work with ddsnap built with ubuntu zlib1g
-echo -n Installing required libraries...
-#apt-get --download-only --reinstall -y -q install zlib1g
-#cp /var/cache/apt/archives/zlib1g_*.deb /mnt
-#chroot /mnt dpkg -i zlib1g_*.deb
-#rm /mnt/zlib1g_*.deb
 echo -e "done.\n"
 
 # Turn off unnecessary services
@@ -38,17 +30,20 @@ rm -f /mnt/etc/rc2.d/{S20exim4,S20openbsd-inetd}
 
 # dmsetup, tree
 echo -n Installing required utilities...
-chroot /mnt dpkg -s dmsetup >& /dev/null || chroot /mnt apt-get -y -q install dmsetup || { umount /mnt; exit 1; }
-chroot /mnt dpkg -s tree >& /dev/null || chroot /mnt apt-get -y -q install tree || { umount /mnt; exit 1; }
+chroot /mnt dpkg -s dmsetup >& /dev/null || chroot /mnt apt-get -y -q install dmsetup >& /dev/null || { umount /mnt; exit 1; }
+chroot /mnt dpkg -s tree >& /dev/null || chroot /mnt apt-get -y -q install tree >& /dev/null || { umount /mnt; exit 1; }
 echo -e "done.\n"
 
 echo -n Upgrading ddsnap and zumastor...
-chroot /mnt wget -q http://zumastor.org/debs/$ZUMA_RELEASE/ddsnap_${ZUMA_RELEASE}_i386.deb
-chroot /mnt wget -q http://zumastor.org/debs/$ZUMA_RELEASE/zumastor_${ZUMA_RELEASE}_i386.deb
-chroot /mnt dpkg -i ddsnap_${ZUMA_RELEASE}_i386.deb >& /dev/null || { umount /mnt; exit 1; }
-chroot /mnt dpkg -i zumastor_${ZUMA_RELEASE}_i386.deb >& /dev/null || { umount /mnt; exit 1; }
-rm -f /mnt/ddsnap_${ZUMA_RELEASE}_i386.deb /mnt/zumastor_${ZUMA_RELEASE}_i386.deb
+DDSNAP_DPKG=`ls ddsnap_*.deb`
+ZUMASTOR_DPKG=`ls zumastor_*.deb`
+mv *.deb /mnt
+chroot /mnt dpkg -i $DDSNAP_DPKG >& /dev/null || { umount /mnt; exit 1; }
+chroot /mnt dpkg -i $ZUMASTOR_DPKG >& /dev/null || { umount /mnt; exit 1; }
+rm -f /mnt/*.deb
 echo -e "done.\n"
+
+chroot /mnt apt-get clean >& /dev/null
 
 echo -n Setting up ssh...
 mkdir -p /mnt/root/.ssh
