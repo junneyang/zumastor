989bfb299675883cff7b07c6368ecc5273584b3d
daniel.r.kegel <daniel.r.kegel@9f37e8af-d821-0410-9430-6fa8b24559eb>
2007-11-21 00:32:27 +0000

    Possible all-in-one script showing how to invoke all tests

---
+++ test/uml/demo.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# Demostrates how to run all UML-based Zumastor tests
+set -e
+set -x
+
+# One-time setup common to all tests
+. config_uml
+test -e  $ZUMA_REPOSITORY/build/linux-${KERNEL_VERSION}/linux || sh build_uml.sh
+test -e uml_fs1 || sh build_fs.sh uml_fs1 && sudo sh build_fs_root.sh uml_fs1
+
+# Just run each test once through, for a quick sanity check
+ITERATIONS=1
+export ITERATIONS
+
+# Single node tests
+sudo sh setup_network_root.sh 192.168.100.1 192.168.100.111 uml_1 uml_fs1
+. config_single
+sh test_target_stop.sh
+
+# Two-node tests
+cp uml_fs1 uml_fs2
+chmod a+rw uml_fs2
+sudo sh setup_network_root.sh 192.168.100.2 192.168.100.111 uml_2 uml_fs2
+. config_replication
+sh test_agent_die.sh
+sh test_fullreplication.sh
+sh test_replication_unplug.sh
+sh test_source_stop.sh
+
+echo All tests complete.
