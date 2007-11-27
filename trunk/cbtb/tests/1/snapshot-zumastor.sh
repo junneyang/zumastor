b9476da21c9217f2eafd5893f714bfb5511edb0b
drake.diedrich <drake.diedrich@9f37e8af-d821-0410-9430-6fa8b24559eb>
2007-11-27 23:40:22 +0000

    - Loop over the standard filesystems ext2, ext3, xfs, jfs, and reiser4. - zumastor stop master testvol at the end of each loop, so everything resets for the next filesystems.  This will check zumastor forget master that it really does clean everything up.

--- trunk/cbtb/tests/1/snapshot-zumastor.sh
+++ trunk/cbtb/tests/1/snapshot-zumastor.sh
@@ -11,72 +11,80 @@
 
 set -e
 
-# Terminate test in 10 minutes.  Read by test harness.
+# Terminate test in 20 minutes.  Read by test harness.
 TIMEOUT=1200
 
 # necessary at the moment, looks like a zumastor bug
 SLEEP=5
 
+
 echo "1..6"
 
-lvcreate --size 1m -n test sysvg
-lvcreate --size 2m -n test_snap sysvg
+lvcreate --size 16m -n test sysvg
+lvcreate --size 32m -n test_snap sysvg
 zumastor define volume testvol /dev/sysvg/test /dev/sysvg/test_snap --initialize
-mkfs.ext3 /dev/mapper/testvol
-zumastor define master testvol -h 24 -d 7
 
-echo ok 1 - testvol set up
+for fstype in ext3 ext2 xfs jfs reiser4
+do
 
-sync ; zumastor snapshot testvol hourly 
+  mkfs.$fstype /dev/mapper/testvol
+  zumastor define master testvol -h 24 -d 7
 
-date >> /var/run/zumastor/mount/testvol/testfile
-sleep $SLEEP
+  echo ok 1 - testvol set up
 
-if [ -d /var/run/zumastor/mount/testvol\(0\)/ ] ; then
-  echo "ok 2 - first snapshot mounted"
-else
-  ls -lR /var/run/zumastor/mount
-  echo "not ok 2 - first snapshot mounted"
-  exit 2
-fi
+  sync ; zumastor snapshot testvol hourly 
 
-if [ ! -f /var/run/zumastor/mount/testvol\(0\)/testfile ] ; then
-  echo "ok 3 - testfile not present in first snapshot"
-else
-  ls -lR /var/run/zumastor/mount
-  echo "not ok 3 - testfile not present in first snapshot"
-  exit 3
-fi
+  date >> /var/run/zumastor/mount/testvol/testfile
+  sleep $SLEEP
 
-sync ; zumastor snapshot testvol hourly 
-sleep $SLEEP
+  if [ -d /var/run/zumastor/mount/testvol\(0\)/ ] ; then
+    echo "ok 2 - first snapshot mounted"
+  else
+    ls -lR /var/run/zumastor/mount
+    echo "not ok 2 - first snapshot mounted"
+    exit 2
+  fi
 
-if [ -d /var/run/zumastor/mount/testvol\(2\)/ ] ; then
-  echo "ok 4 - second snapshot mounted"
-else
-  ls -lR /var/run/zumastor/mount
-  echo "not ok 4 - second snapshot mounted"
-  exit 4
-fi
+  if [ ! -f /var/run/zumastor/mount/testvol\(0\)/testfile ] ; then
+    echo "ok 3 - testfile not present in first snapshot"
+  else
+    ls -lR /var/run/zumastor/mount
+    echo "not ok 3 - testfile not present in first snapshot"
+    exit 3
+  fi
 
-if diff -q /var/run/zumastor/mount/testvol/testfile \
-    /var/run/zumastor/mount/testvol\(2\)/testfile 2>&1 >/dev/null ; then
-  echo "ok 5 - identical testfile immediately after second snapshot"
-else
-  ls -lR /var/run/zumastor/mount
-  echo "not ok 5 - identical testfile immediately after second snapshot"
-  exit 5
-fi
+  sync ; zumastor snapshot testvol hourly 
+  sleep $SLEEP
 
-date >> /var/run/zumastor/mount/testvol/testfile
+  if [ -d /var/run/zumastor/mount/testvol\(2\)/ ] ; then
+    echo "ok 4 - second snapshot mounted"
+  else
+    ls -lR /var/run/zumastor/mount
+    echo "not ok 4 - second snapshot mounted"
+    exit 4
+  fi
 
-if ! diff -q /var/run/zumastor/mount/testvol/testfile \
-    /var/run/zumastor/mount/testvol\(2\)/testfile 2>&1 >/dev/null ; then
-  echo "ok 6 - testfile changed between origin and second snapshot"
-else
-  ls -lR /var/run/zumastor/mount
-  echo "not ok 6 - testfile changed between origin and second snapshot"
-  exit 6
-fi
+  if diff -q /var/run/zumastor/mount/testvol/testfile \
+      /var/run/zumastor/mount/testvol\(2\)/testfile 2>&1 >/dev/null ; then
+    echo "ok 5 - identical testfile immediately after second snapshot"
+  else
+    ls -lR /var/run/zumastor/mount
+    echo "not ok 5 - identical testfile immediately after second snapshot"
+    exit 5
+  fi
+
+  date >> /var/run/zumastor/mount/testvol/testfile
+
+  if ! diff -q /var/run/zumastor/mount/testvol/testfile \
+      /var/run/zumastor/mount/testvol\(2\)/testfile 2>&1 >/dev/null ; then
+    echo "ok 6 - testfile changed between origin and second snapshot"
+  else
+    ls -lR /var/run/zumastor/mount
+    echo "not ok 6 - testfile changed between origin and second snapshot"
+    exit 6
+  fi
+
+  zumastor forget master testvol
+done
 
 exit 0
