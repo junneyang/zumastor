abb04a3e7fc38c671a9f618cdb23b3e967dd23d7
drake.diedrich <drake.diedrich@9f37e8af-d821-0410-9430-6fa8b24559eb>
2007-12-12 22:57:57 +0000

    - re-enable -loadvm running, to make use of the pre-snapshotted running  virtual machine - specify 512M ram to the virtual machine, matching the installed vm.

--- cbtb/host-scripts/test-zuma-dapper-i386.sh
+++ cbtb/host-scripts/test-zuma-dapper-i386.sh
@@ -123,7 +123,8 @@ fi
 
 # TODO(dld): add back  -loadvm running  when method to deal with changed
 # ethernet/IP allocations is developed.
-${rqemu_i386} \
+${rqemu_i386} -m 512 \
+  -loadvm running \
   -serial file:${SERIAL} \
   -monitor unix:${MONITOR},server,nowait \
   -vnc unix:${VNC} \
@@ -136,7 +137,8 @@ if [ "x$MACADDR2" != "x" ] ; then
   MONITOR2=${tmpdir}/monitor2
   VNC2=${tmpdir}/vnc2
   # TODO(dld): See above.  -loadvm running
-  ${rqemu_i386} \
+  ${rqemu_i386} -m 512 \
+    -loadvm running \
     -serial file:${SERIAL2} \
     -monitor unix:${MONITOR2},server,nowait \
     -vnc unix:${VNC2} \
