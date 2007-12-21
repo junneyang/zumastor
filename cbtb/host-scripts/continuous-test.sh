14f561087b784636ed5c0c736a0e98ce4d866ce9
drake.diedrich <drake.diedrich@9f37e8af-d821-0410-9430-6fa8b24559eb>
2007-12-21 21:19:06 +0000

    Breakup the continuous-test loop and test runner
    - reuse the same tunbr variables for all test runs, which will be
    required to use the qemu snapshoting capability in a later patch
    - separate the test running/reporting code from the loop and
    decision-to-run, with tunbr invoked from the outer loop.
    - will also help running multiple tests simultaneously eventually

--- cbtb/host-scripts/continuous-test.sh
+++ cbtb/host-scripts/continuous-test.sh
@@ -8,7 +8,6 @@
 # install revision, fire off a new round of tests.
 
 top="${PWD}"
-branch=`cat $top/zumastor/Version`
 
 installrev=''
 if [ -f ${top}/zumastor/build/installrev ] ; then
@@ -39,149 +38,20 @@ if [ "x$FAILED_TEST_REV" = "x$installrev" ] ; then
 fi
           
           
-mailto=/usr/bin/mailto
-sendmail=/usr/sbin/sendmail
-biabam=/usr/bin/biabam
 TUNBR=tunbr
-email_failure="zumastor-commits@googlegroups.com"
-email_success="zumastor-commits@googlegroups.com"
 repo="${top}/zumastor-tests"
-export DISKIMG="${top}/zumastor/build/dapper-i386-zumastor-r$installrev.img"
-export LOGDIR="${top}/zumastor/build/r$installrev"
-[ -d $LOGDIR ] || mkdir $LOGDIR
-
-summary=${LOGDIR}/summary
 
 [ -x /etc/default/testenv ] && . /etc/default/testenv
 
 
-
-
-if [ ! -d ${repo} ] ; then
-  echo "svn checkout http://zumastor.googlecode.com/svn/trunk/cbtb/tests zumastor-tests"
-  echo "cp $0 to the parent directory of the zumastor-tests repository and "
-  echo "run from that location.  Periodically inspect the cbtb/host-scripts"
-  echo "for changes and redeploy them."
-  exit 1
-fi
-
-pushd ${repo}
-
-# build and test the last successfully installed revision
-svn update -r $installrev
-
-testret=0
-
-pushd 1
-for f in *.sh
-do
-  # timeout any test that runs for more than an hour
-  export LOGPREFIX="$f."
-  testlog="${LOGDIR}/${LOGPREFIX}log"
-  ${TUNBR} timeout -14 3600 ${top}/test-zuma-dapper-i386.sh $f >${testlog} 2>&1
-  testrc=$?
-  files="$testlog $files"
-  if [ $testrc -eq 0 ]
-  then
-    echo PASS $f >>$summary
-  else
-    if egrep '^EXPECT_FAIL=1' ./${f} ; then
-      echo FAIL "$f*" >>$summary
-    else
-      testret=$testrc
-      echo FAIL $f >>$summary
-    fi
-
-    if [ -f "${LOGDIR}/${LOGPREFIX}screen.png" ] ; then
-      files="${LOGDIR}/${LOGPREFIX}screen.png $files"
-    fi
-
-  fi
-done
-popd
-
-pushd 2
-for f in *.sh
-do
-  export LOGPREFIX="$f."
-  testlog="${LOGDIR}/${LOGPREFIX}log"
-  ${TUNBR} ${TUNBR} timeout -14 3600 ${top}/test-zuma-dapper-i386.sh $f >${testlog} 2>&1
-  testrc=$?
-  files="$testlog $files"
-  if  [ $testrc -eq 0 ]
-  then
-    echo PASS $f >>$summary
-  else
-    if egrep '^EXPECT_FAIL=1' ./${f} ; then
-      echo FAIL "$f*" >>$summary
-    else
-      testret=$testrc
-      echo FAIL $f >>$summary
-    fi
-
-    if [ -f "${LOGDIR}/$LOGPREFIX}screen.png" ] ; then
-      files="${LOGDIR}/$LOGPREFIX}screen.png $files"
-    fi
-    if [ -f "${LOGDIR}/$LOGPREFIX}screen2.png" ] ; then
-      files="${LOGDIR}/$LOGPREFIX}screen2.png $files"
-    fi
-
-  fi
-done
-popd
-
-popd
-    
-# send summary, logs, and a success or failure subject to the
-# success or failure mailing lists
-if [ $testret -eq 0 ]; then
-  subject="zumastor b$branch r$installrev test success"
-  email="${email_success}"
-
-  # update the presistent revision number of the last revision that passed
-  # all required tests
-  echo $installrev >${top}/zumastor/build/testrev.new
-  mv ${top}/zumastor/build/testrev.new ${top}/zumastor/build/testrev
-
+if $TUNBR $TUNBR ./runtests.sh
+then
+  export EXPECT_FAIL_REV=
 else
-  subject="zumastor b$branch r$installrev test failure $testret"
-  email="${email_failure}"
-  export FAILED_TEST_REV=$installrev
+  export EXPECT_FAIL_REV=$installrev
 fi
 
-
-# send $subject and $files to $email
-if [ -x ${mailto} ] ; then
-  (
-    cat $summary
-    for f in $files
-    do
-      echo '~*'
-      echo 1
-      echo $f
-      echo text/plain
-    done
-  ) | ${mailto} -s "${subject}" ${email}
-
-elif [ -x ${biabam} ] ; then
-  bfiles=`echo $files | tr ' ' ','`
-  cat $summary | ${biabam} $bfiles -s "${subject}" ${email}
-
-elif [ -x ${sendmail} ] ; then
-  (
-    echo "Subject: " $subject
-    echo
-    cat $summary
-    for f in $files
-    do
-      echo
-      echo $f
-      echo "------------------"
-      cat $f
-    done
-  ) | ${sendmail} ${email}
-fi
-
-# loop and reload the script
+# Perhaps shorten this if a test ran successfully, as that caused
+# a delay already on it's own.  
 sleep 300
 exec $0
