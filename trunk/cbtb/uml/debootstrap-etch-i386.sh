d710a8593fef28a5622959fefa97c231fe091576
drake.diedrich <drake.diedrich@9f37e8af-d821-0410-9430-6fa8b24559eb>
2008-01-01 16:56:02 +0000

    Build userspace packages and UML test kernel natively on Debian/etch i386 Takes 12 minutes for build on T60p (2x2.16GHz Core).

---
+++ trunk/cbtb/uml/debootstrap-etch-i386.sh
@@ -0,0 +1,109 @@
+#!/bin/sh -x
+
+#
+# Make a tarball of the root system of a basic Ubunutu/dapper system,
+# for use in preparing UML root partition images
+#
+
+# $Id$
+# Copyright 2007 Google Inc.
+# Author: Drake Diedrich <dld@google.com>
+# License: GPLv2
+
+set -e
+
+pushd ../..
+SRC=$PWD
+BUILD_DIR="$SRC/build"
+if [ -f SVNREV ] ; then
+  SVNREV=`awk '/^[0-9]+$/ { print $1; }' SVNREV`
+else
+  SVNREV=`svn info zumastor | grep ^Revision:  | cut -d\  -f2`
+fi
+popd
+
+ARCH=i386
+DIST=etch
+DEBOOTSTRAP=/usr/sbin/debootstrap
+SUDO=sudo
+
+# the testenv setup, see cbtb/host-setup/
+VIRTHOST=192.168.23.1
+[ -x /etc/default/testenv ] && . /etc/default/testenv
+
+rootdir=`mktemp -d`
+stagefile=`mktemp`
+
+TESTDEPENDENCIES=openssh-server,cron,postfix,dmsetup
+BUILDDEPEDENCIES=build-essential,lvm2,fakeroot,kernel-package,devscripts,subversion,debhelper,libpopt-dev,zlib1g-dev,debhelper,bzip2
+EXCLUDE=alsa-base,alsa-utils,eject,console-data,libasound2,linux-sound-base,memtest86,mii-diag,module-init-tools,wireless-tools,wpasupplicant,pcmciautils
+
+# Basic dapper software installed into $rootdir
+#  --exclude=$EXCLUDE
+
+$SUDO $DEBOOTSTRAP --arch $ARCH \
+  --include=$TESTDEPENDENCIES,$BUILDDEPEDENCIES \
+  $DIST $rootdir http://$VIRTHOST/debian
+
+# create and authorize a local ssh key for root
+$SUDO mkdir -p $rootdir/root/.ssh
+$SUDO ssh-keygen -q -P '' -N '' -t dsa -f $rootdir/root/.ssh/id_dsa
+$SUDO cat $rootdir/root/.ssh/id_dsa.pub >$stagefile
+
+# Authorize the user to ssh into this virtual instance
+cat ~/.ssh/*.pub >>$stagefile
+$SUDO mv $stagefile $rootdir/root/.ssh/authorized_keys
+$SUDO chown root:root $rootdir/root/.ssh/authorized_keys
+$SUDO chmod 600 $rootdir/root/.ssh/authorized_keys
+
+
+echo unassigned >$stagefile
+$SUDO mv $stagefile $rootdir/etc/hostname
+
+
+cat >$stagefile <<EOF
+# Used by ifup(8) and ifdown(8). See the interfaces(5) manpage or
+# /usr/share/doc/ifupdown/examples for more information.
+
+# The loopback network interface
+auto lo
+iface lo inet loopback
+
+# The primary network interface
+auto eth0
+iface eth0 inet dhcp
+EOF
+$SUDO mv $stagefile $rootdir/etc/network/interfaces
+
+# clean up the downloaded packages to conserve space
+$SUDO rm -f $rootdir/var/cache/apt/archives/*.deb
+
+# create and mount an empty ext3 filesystem
+ext3dev=`mktemp`
+ext3dir=`mktemp -d`
+dd if=/dev/zero bs=1M seek=1024 count=0 of=$ext3dev
+$SUDO mkfs.ext3 -F $ext3dev
+$SUDO mount -oloop,rw $ext3dev $ext3dir
+
+# tar it up the new filesystem onto the new ext3 device
+$SUDO tar cf - -C $rootdir . | \
+  $SUDO tar xf - -C $ext3dir
+
+# create symlinks from sd* to the ubd* devices
+$SUDO ln -s ubda $ext3dir/dev/sda
+$SUDO ln -s ubdb $ext3dir/dev/sdb
+$SUDO ln -s ubdc $ext3dir/dev/sdc
+$SUDO ln -s ubdd $ext3dir/dev/sdd
+
+
+$SUDO umount $ext3dir
+
+# copy it back into the build/ directory, avoiding NFS issues and
+# potential races.
+$SUDO chown $USER $ext3dev
+mv $ext3dev $BUILD_DIR/$DIST-$ARCH-r$SVNREV.ext3
+ln -sf $DIST-$ARCH-r$SVNREV.ext3 $BUILD_DIR/$DIST-$ARCH.ext3 
+
+# cleanup
+$SUDO rm -rf $rootdir
+$SUDO rmdir $ext3dir
