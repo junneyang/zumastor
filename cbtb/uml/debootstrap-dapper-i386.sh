d6d7bc6c3244c4b7f4a2ee904e646a7260a9b15a
drake.diedrich <drake.diedrich@9f37e8af-d821-0410-9430-6fa8b24559eb>
2008-01-01 06:26:02 +0000

    parameterize and qualify locations of debootstrap and sudo Add an excluded package list to prune unnecessary packages like asound Stage files to /tmp to work better in NFS environments Move mkdir /root/.ssh ahead where needed for key generation.

--- cbtb/uml/debootstrap-dapper-i386.sh
+++ cbtb/uml/debootstrap-dapper-i386.sh
@@ -23,6 +23,8 @@ fi
 popd
 
 ARCH=i386
+DEBOOTSTRAP=/usr/sbin/debootstrap
+SUDO=sudo
 
 # the testenv setup, see cbtb/host-setup/
 VIRTHOST=192.168.23.1
@@ -33,25 +35,29 @@ stagefile=`mktemp`
 
 TESTDEPENDENCIES=openssh-server,cron,postfix,dmsetup
 BUILDDEPEDENCIES=build-essential,lvm2,fakeroot,kernel-package,devscripts,subversion,debhelper,libpopt-dev,zlib1g-dev,debhelper,bzip2
+EXCLUDE=alsa-base,alsa-utils,eject,console-data,libasound2,linux-sound-base,memtest86,mii-diag,module-init-tools,wireless-tools,wpasupplicant,pcmciautils
 
 # Basic dapper software installed into $rootdir
-sudo debootstrap --arch $ARCH \
+$SUDO $DEBOOTSTRAP --arch $ARCH \
   --include=$TESTDEPENDENCIES,$BUILDDEPEDENCIES \
+  --exclude=$EXCLUDE \
   dapper $rootdir http://$VIRTHOST/ubuntu
+
+# create and authorize a local ssh key for root
+$SUDO mkdir -p $rootdir/root/.ssh
+$SUDO ssh-keygen -q -P '' -N '' -t dsa -f $rootdir/root/.ssh/id_dsa
+$SUDO cat $rootdir/root/.ssh/id_dsa.pub >$stagefile
 
 # Authorize the user to ssh into this virtual instance
 cat ~/.ssh/*.pub >>$stagefile
-sudo mkdir -p $rootdir/root/.ssh/
-sudo mv $stagefile $rootdir/root/.ssh/authorized_keys
-sudo chown root:root $rootdir/root/.ssh/authorized_keys
-sudo chmod 600 $rootdir/root/.ssh/authorized_keys
+$SUDO mv $stagefile $rootdir/root/.ssh/authorized_keys
+$SUDO chown root:root $rootdir/root/.ssh/authorized_keys
+$SUDO chmod 600 $rootdir/root/.ssh/authorized_keys
 
 
 echo unassigned >$stagefile
-sudo mv $stagefile $rootdir/etc/hostname
+$SUDO mv $stagefile $rootdir/etc/hostname
 
-sudo ssh-keygen -q -P '' -N '' -t dsa -f $rootdir/root/.ssh/id_dsa
-sudo "cat $rootdir/root/.ssh/id_dsa.pub >> $rootdir/root/.ssh/authorized_keys"
 
 cat >$stagefile <<EOF
 # Used by ifup(8) and ifdown(8). See the interfaces(5) manpage or
@@ -65,43 +71,43 @@ iface lo inet loopback
 auto eth0
 iface eth0 inet dhcp
 EOF
-sudo mv $stagefile $rootdir/etc/network/interfaces
+$SUDO mv $stagefile $rootdir/etc/network/interfaces
 
 # clean up the downloaded packages to conserve space
-sudo rm -f $rootdir/var/cache/apt/archives/*.deb
+$SUDO rm -f $rootdir/var/cache/apt/archives/*.deb
 
 # create and mount an empty ext3 filesystem
 ext3dev=`mktemp`
 ext3dir=`mktemp -d`
 dd if=/dev/zero bs=1M seek=1024 count=0 of=$ext3dev
-sudo mkfs.ext3 -F $ext3dev
-sudo mount -oloop,rw $ext3dev $ext3dir
+$SUDO mkfs.ext3 -F $ext3dev
+$SUDO mount -oloop,rw $ext3dev $ext3dir
 
 # tar it up the new filesystem onto the new ext3 device
-sudo tar cf - -C $rootdir . | \
-  sudo tar xf - -C $ext3dir
+$SUDO tar cf - -C $rootdir . | \
+  $SUDO tar xf - -C $ext3dir
 
 # create symlinks from sd* to the ubd* devices
-sudo ln -s ubda $ext3dir/dev/sda
-sudo ln -s ubdb $ext3dir/dev/sdb
-sudo ln -s ubdc $ext3dir/dev/sdc
-sudo ln -s ubdd $ext3dir/dev/sdd
+$SUDO ln -s ubda $ext3dir/dev/sda
+$SUDO ln -s ubdb $ext3dir/dev/sdb
+$SUDO ln -s ubdc $ext3dir/dev/sdc
+$SUDO ln -s ubdd $ext3dir/dev/sdd
 cat >$stagefile <<EOF
 KERNEL=="ubda",                 SYMLINK+="sda"
 KERNEL=="ubdb",                 SYMLINK+="sdb"
 KERNEL=="ubdc",                 SYMLINK+="sdc"
 KERNEL=="ubdd",                 SYMLINK+="sdd"
 EOF
-sudo mv $stagefile $ext3dir/etc/udev/rules.d/61-ubd-symlinks.rules
+$SUDO mv $stagefile $ext3dir/etc/udev/rules.d/61-ubd-symlinks.rules
 
-sudo umount $ext3dir
+$SUDO umount $ext3dir
 
 # copy it back into the build/ directory, avoiding NFS issues and
 # potential races.
-sudo chown $USER $ext3dev
+$SUDO chown $USER $ext3dev
 mv $ext3dev $BUILD_DIR/dapper-$ARCH-r$SVNREV.ext3
 ln -sf dapper-$ARCH-r$SVNREV.ext3 $BUILD_DIR/dapper-$ARCH.ext3 
 
 # cleanup
-sudo rm -rf $rootdir
-sudo rmdir $ext3dir
+$SUDO rm -rf $rootdir
+$SUDO rmdir $ext3dir
