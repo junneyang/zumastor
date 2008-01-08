f40ed3d1b2649b3efc2508df2b1d03387b4277ce
drake.diedrich <drake.diedrich@9f37e8af-d821-0410-9430-6fa8b24559eb>
2008-01-08 23:21:16 +0000

    - Document waiting at end of a single CBT/UML test case. - Try new synchronous --wait in replication-zumastor test. - Extra filesystem reports in replication-zumastor test.

--- cbtb/tests/2/replication-zumastor.sh
+++ cbtb/tests/2/replication-zumastor.sh
@@ -28,10 +28,37 @@ SSH='ssh -o StrictHostKeyChecking=no -o BatchMode=yes'
 SCP='scp -o StrictHostKeyChecking=no -o BatchMode=yes'
 
 
-# necessary at the moment, looks like a zumastor bug
-SLEEP=5
+# wait for file.  The first argument is the timeout, the second the file.
+timeout_file_wait() {
+  local max=$1
+  local file=$2
+  local count=0
+  while [ ! -e $file ] && [ $count -lt $max ]
+  do 
+    let "count = count + 1"
+    sleep 1
+  done
+  [ -e $file ]
+  return $?
+}
 
-echo "1..4"
+# wait for file on remote site with timeout.
+timeout_remote_file_wait() {
+  local max=$1
+  local remote=$2
+  local file=$3
+  local count=0
+  while $SSH $remote [ ! -e $file ] && [ $count -lt $max ]
+  do 
+    let "count = count + 1"
+    sleep 1
+  done
+  $SSH $remote [ -e $file ]
+  return $?
+}
+
+
+echo "1..10"
 
 echo ${IPADDR} master >>/etc/hosts
 echo ${IPADDR2} slave >>/etc/hosts
@@ -64,22 +91,39 @@ ${SSH} root@${slave} zumastor start source testvol
 ${SSH} root@${slave} zumastor status --usage
 echo ok 5 - replication started on slave
 
-zumastor replicate testvol slave
+zumastor replicate testvol --wait slave
 zumastor status --usage
-echo ok 6 - replication manually kicked off from master
 
 # reasonable wait for these small volumes to finish the initial replication
-${SSH} root@${slave} ls -l /var/run/zumastor/mount/testvol || true
+if ! timeout_remote_file_wait 120 root@${slave} /var/run/zumastor/mount/testvol
+then
+  $SSH root@${slave} "df -h ; mount"
+  $SSH root@${slave} ls -alR /var/run/zumastor
+  $SSH root@${slave} zumastor status --usage
+  $SSH root@${slave} tail -200 /var/log/syslog
+  
+  echo not ok 6 - replication manually from master
+  exit 6
+else
+  echo ok 6 - replication manually from master
+fi
+
+
 
 date >>/var/run/zumastor/mount/testvol/testfile
 sync
 zumastor snapshot testvol hourly 
-sleep 2
-zumastor status --usage
-${SSH} root@${slave} ls -l /var/run/zumastor/mount/testvol || true
-${SSH} root@${slave} zumastor status --usage
 
-echo ok 7 - testfile written, synced, and snapshotted
+if ! timeout_file_wait 30 /var/run/zumastor/mount/testvol
+then
+  ls -alR /var/run/zumastor
+  zumastor status --usage
+  tail -200 /var/log/syslog
+  echo not ok 7 - testfile written, synced, and snapshotted
+  exit 7
+else
+  echo ok 7 - testfile written, synced, and snapshotted
+fi
 
 hash=`md5sum /var/run/zumastor/mount/testvol/testfile`
 
@@ -89,28 +133,58 @@ hash=`md5sum /var/run/zumastor/mount/testvol/testfile`
 zumastor replicate testvol slave
 
 
-# give it a minute to replicate (on a 30 second cycle), and verify
-# that it is there.  If not, look at the target volume, wait 5 minutes,
-# and look again
-sleep 60
+# give it two minutes to replicate (on a 30 second cycle), and verify
+# that it is there.  If not, look at the target volume
+if ! timeout_remote_file_wait 120 root@${slave} /var/run/zumastor/mount/testvol
+then
+  $SSH root@${slave} ls -alR /var/run/zumastor
+  $SSH root@${slave} zumastor status --usage
+  $SSH root@${slave} tail -200 /var/log/syslog
+  
+  echo not ok 8 - testvol has migrated to slave
+  exit 8
+else
+  echo ok 8 - testvol has migrated to slave
+fi
+
+# check separately for the testfile
+if ! timeout_remote_file_wait 120 root@${slave} /var/run/zumastor/mount/testfile
+then
+  $SSH root@${slave} ls -alR /var/run/zumastor
+  $SSH root@${slave} zumastor status --usage
+  $SSH root@${slave} tail -200 /var/log/syslog
+  
+  echo not ok 9 - testfile has migrated to slave
+  exit 9
+else
+  echo ok 9 - testfile has migrated to slave
+fi
+
 rhash=`${SSH} root@${slave} md5sum /var/run/zumastor/mount/testvol/testfile` || \
   ${SSH} root@${slave} <<EOF
     mount
     df
-    ls -lR /var/run/zumastor/mount/
+    ls -lR /var/run/zumastor/
     tail -200 /var/log/syslog
-    sleep 300
-    mount
-    df
-    ls -lR /var/run/zumastor/mount/
 EOF
 
 
 if [ "$rhash" = "$hash" ] ; then
-  echo ok 8 - origin and slave testfiles are in sync
+  echo ok 10 - origin and slave testfiles are in sync
 else
-  echo not ok 8 - origin and slave testfiles are in sync
-  exit 8
+  echo not ok 10 - origin and slave testfiles are in sync
+    mount
+    df
+    ls -lR /var/run/zumastor/
+    tail -200 /var/log/syslog
+  ${SSH} root@${slave} <<EOF
+    mount
+    df
+    zumastor status --usage
+    ls -lR /var/run/zumastor/
+    tail -200 /var/log/syslog
+EOF
+  exit 10
 fi
 
 exit 0
