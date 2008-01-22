e7caedac0812c6de759e851917d8b0bac0675372
jiahotcake <jiahotcake@9f37e8af-d821-0410-9430-6fa8b24559eb>
2008-01-22 20:16:47 +0000

    Turn on all the correctness checkings of zumastor origin/snapshot resizing. The test should pass with the two lastest revisions 1296 and 1297.

--- cbtb/tests/1/snapshot-zumastor-lvm-ext3-resize.sh
+++ cbtb/tests/1/snapshot-zumastor-lvm-ext3-resize.sh
@@ -57,6 +57,21 @@ function size_check {
   fi
 }
 
+function snapshot_size_check {
+  local -r id=$1
+  if [ $id -eq -1 ]; then
+    snapsize=`blockdev --getsize /dev/mapper/testvol`
+  else
+    snapsize=`blockdev --getsize /dev/mapper/testvol\($id\)`
+  fi
+  if [ $snapsize = $2 ]; then
+    echo "ok $3 - $4"
+  else
+    echo "not ok $3 - $4"
+    exit $3
+  fi
+}
+
 apt-get update
 aptitude install -y e2fsprogs
 
@@ -68,7 +83,7 @@ time vgcreate testvg /dev/sdb
 time lvcreate --size 8M -n test testvg
 time lvcreate --size 8M -n test_snap testvg
 
-echo "1..10"
+echo "1..17"
 
 zumastor define volume testvol /dev/testvg/test /dev/testvg/test_snap --initialize
 
@@ -93,52 +108,58 @@ file_check 3 "identical testfile after snapshot"
 
 # test origin volume enlarge
 lvresize /dev/testvg/test -L 16m
-/etc/init.d/zumastor restart
 zumastor stop master testvol
+zumastor resize testvol --origin 16m
 e2fsck -f -y /dev/mapper/testvol
 resize2fs /dev/mapper/testvol 16m
 zumastor start master testvol
 size_check "origin" "16,777,216" 4 "size check after origin volume enlarge"
 file_check 5 "testfile changed after origin volume enlarge"
+# the size of the old snapshot should not change
+snapshot_size_check -1 32768 6 "origin volume size check after origin volume enlarge"
+snapshot_size_check 0 16384 7 "old snapshot size check after origin volume enlarge"
 
 # test snapshot store enlarge
 lvresize /dev/testvg/test_snap -L 64m
-/etc/init.d/zumastor restart
+zumastor resize testvol --snapshot 64m
 # 1024 is the number of snapshot blocks with the defaul block size 16k
-size_check "snap" "4,096" 6 "size check after snapshot store enlarge"
-file_check 7 "testfile changed after snapshot store enlarge"
+size_check "snap" "4,096" 8 "size check after snapshot store enlarge"
+file_check 9 "testfile changed after snapshot store enlarge"
 # we will have two snapshots mounted if snapshot enlarging succeeds
 # otherwise, ddsnap server will automatically delete the old snapshot
 dd if=/dev/zero of=/var/run/zumastor/mount/testvol/zerofile bs=1k count=12k
 sync
 zumastor snapshot testvol hourly
 if timeout_file_wait 120 /var/run/zumastor/snapshot/testvol/hourly.1 ; then
-  echo "ok 8 - two snapshots are mounted"
+  echo "ok 10 - two snapshots are mounted"
 else
   ls -laR /var/run/zumastor/mount
-  echo "not ok 8 - two snapshots are mounted"
+  echo "not ok 10 - two snapshots are mounted"
   exit 2
 fi
 rm -f /var/run/zumastor/mount/testvol/zerofile
 sync
 
-# test origin volume shrink: currently not fully supported
+# test origin volume shrink
 zumastor stop master testvol
 e2fsck -f -y /dev/mapper/testvol
 resize2fs /dev/mapper/testvol 4m
 # force any data left on the freed space to be copied out to the snapstore
 dd if=/dev/zero of=/dev/mapper/testvol bs=1k seek=4k count=12k
 sync
-/etc/init.d/zumastor stop
+zumastor resize testvol --origin 4m
 echo y | lvresize /dev/testvg/test -L 4m
-/etc/init.d/zumastor start
-size_check "origin" "4,194,304" 9 "size check after origin volume shrink"
-#file_check 9 "testfile changed after origin volume shrink"
+zumastor start master testvol
+size_check "origin" "4,194,304" 11 "size check after origin volume shrink"
+file_check 12 "testfile changed after origin volume shrink"
+snapshot_size_check -1 8192 13 "origin volume size check after origin volume shrink"
+snapshot_size_check 0 16384 14 "old snapshot size check after origin volume shrink"
+snapshot_size_check 2 32768 15 "old snapshot size check after origin volume shrink"
 
-# test snapshot store shrink: currently not fully supported
+# test snapshot store shrink
+zumastor resize testvol --snapshot 32m
 echo y | lvresize /dev/testvg/test_snap -L 32m
-/etc/init.d/zumastor restart
-size_check "snap" "2,048" 10 "size check after snapshot store shrink"
-#file_check 11 "testfile changed after snapshot store shrink"
+size_check "snap" "2,048" 16 "size check after snapshot store shrink"
+file_check 17 "testfile changed after snapshot store shrink"
 
 exit 0
