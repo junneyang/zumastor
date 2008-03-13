4aa80d2bdb93676bf33b1e302029262eec0e7f75
drake.diedrich <drake.diedrich@9f37e8af-d821-0410-9430-6fa8b24559eb>
2008-03-13 00:06:30 +0000

    Bring the zbuild startup script into the repository. Remove any repository lock files on boot, as cleanup for reboots during   reboot while working dir is locked.

---
+++ cbtb/host-scripts/init.d
@@ -0,0 +1,27 @@
+#!/bin/sh
+#
+# $Id$
+#
+# Launch the zumastor continuous build script as the zbuild user
+# on bootup.  Should be installed as /etc/init.d/zbuild and
+# symlinked from /etc/rc2.d/S95zbuild
+
+rm -f /var/lib/misc/dnsmasq.leases /var/lib/misc/dnsmasq.leases.new
+
+mkdir /var/run/zbuild
+chown zbuild:zbuild /var/run/zbuild
+
+
+su - zbuild <<EOF
+rm -f zumastor/lock 0.6/zumastor/lock
+LOCKFILE=/var/run/zbuild/build.lock ./continuous-build.sh >>continuous-build.log 2>&1 </dev/null &
+sleep 10
+./continuous-install.sh >>continuous-install.log 2>&1 </dev/null &
+sleep 10
+./continuous-test.sh >>continuous-test.log 2>&1 </dev/null &
+sleep 10
+cd 0.6 && LOCKFILE=/var/run/zbuild/build.lock ./continuous-build.sh >>continuous-build.log 2>&1 </dev/null &
+sleep 10
+cd 0.6 && ./continuous-install.sh >>continuous-install.log 2>&1 </dev/null &
+sleep 10
+EOF
