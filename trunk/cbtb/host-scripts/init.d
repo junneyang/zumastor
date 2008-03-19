dbcb5d1190abd0e05fc306b016dca11d3ca8b604
drake.diedrich <drake.diedrich@9f37e8af-d821-0410-9430-6fa8b24559eb>
2008-03-19 23:50:48 +0000

    - init script had startup problems with cwd. cd only once.

--- trunk/cbtb/host-scripts/init.d
+++ trunk/cbtb/host-scripts/init.d
@@ -6,13 +6,14 @@
 # on bootup.  Should be installed as /etc/init.d/zbuild and
 # symlinked from /etc/rc2.d/S95zbuild
 
-rm -f /var/lib/misc/dnsmasq.leases /var/lib/misc/dnsmasq.leases.new
+case "$1" in
+    start)
+        rm -f /var/lib/misc/dnsmasq.leases /var/lib/misc/dnsmasq.leases.new
 
-mkdir /var/run/zbuild
-chown zbuild:zbuild /var/run/zbuild
-
-
-su - zbuild <<EOF
+        mkdir /var/run/zbuild
+        chown zbuild:zbuild /var/run/zbuild
+        su - zbuild <<EOF
+set -x
 rm -f zumastor/lock 0.6/zumastor/lock
 LOCKFILE=/var/run/zbuild/build.lock ./continuous-build.sh >>continuous-build.log 2>&1 </dev/null &
 sleep 10
@@ -20,8 +21,21 @@ sleep 10
 sleep 10
 ./continuous-test.sh >>continuous-test.log 2>&1 </dev/null &
 sleep 10
-cd 0.6 && LOCKFILE=/var/run/zbuild/build.lock ./continuous-build.sh >>continuous-build.log 2>&1 </dev/null &
+cd 0.6
+LOCKFILE=/var/run/zbuild/build.lock ./continuous-build.sh >>continuous-build.log 2>&1 </dev/null &
 sleep 10
-cd 0.6 && ./continuous-install.sh >>continuous-install.log 2>&1 </dev/null &
+./continuous-install.sh >>continuous-install.log 2>&1 </dev/null &
 sleep 10
+./continuous-test.sh >>continuous-test.log 2>&1 </dev/null &
+jobs
 EOF
+
+        jobs
+        ;;
+
+    stop)
+        rmdir /var/run/zbuild
+        ;;
+esac
+
+exit 0
