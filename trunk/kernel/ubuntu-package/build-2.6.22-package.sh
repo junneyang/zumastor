d99b72c0af485626ec834a6e54ab375c276a9c58
williamanowak <williamanowak@9f37e8af-d821-0410-9430-6fa8b24559eb>
2008-03-21 21:30:50 +0000

    - This is a pretty big change: -- Fork the build-2.6.24 script to build-2.6.22 -- Rename the existing patches directory to patches-2.6.24 -- Create a stub patches directory for 2.6.22 -- Create stub configs for amd64 and lpia with 2.6.22

--- trunk/kernel/ubuntu-package/build-2.6.22-package.sh
+++ trunk/kernel/ubuntu-package/build-2.6.22-package.sh
@@ -1,44 +1,46 @@
 #!/bin/bash -x
-# Build a 2.6.24 kernel package for Zumastor
+# Build a 2.6.22 kernel package for Zumastor
 
-VERSION="2.6.24-12.22"
+VERSION="2.6.22-14.52"
 ZUMAREVISION=`svn info ../../ | grep ^Revision:  | cut -d\  -f2`
 PKGVERSION="$VERSION~zumappa$ZUMAREVISION"
 MIRROR="http://archive.ubuntu.com/ubuntu"
-DISTRIBUTION="hardy"
+DISTRIBUTION="gutsy"
 CHANGELOG="Upstream Package with Zumastor.org r$ZUMAREVISION patches"
 
 WORKDIR=`mktemp -d`
 
-wget $MIRROR/pool/main/l/linux/linux_2.6.24.orig.tar.gz -O \
-  $WORKDIR/linux_2.6.24.orig.tar.gz
-wget $MIRROR/pool/main/l/linux/linux_2.6.24-12.22.dsc -O \
-  $WORKDIR/linux_2.6.24-12.22.dsc
-wget $MIRROR/pool/main/l/linux/linux_2.6.24-12.22.diff.gz -O \
-  $WORKDIR/linux_2.6.24-12.22.diff.gz
+MIRRORDIR="$MIRROR/pool/main/l/linux-source-2.6.22"
+for file in linux-source-2.6.22_2.6.22.orig.tar.gz \
+            linux-source-2.6.22_2.6.22-14.52.dsc \
+            linux-source-2.6.22_2.6.22-14.52.diff.gz
+do
+  wget $MIRRORDIR/$file -O $WORKDIR/$file
+done
 
 CWD=`pwd`
 cd $WORKDIR
-dpkg-source -x linux_2.6.24-12.22.dsc
-cd linux-2.6.24
-for patchfile in $CWD/patches-2.6.24/*.patch
+dpkg-source -x linux-source-2.6.22_2.6.22-14.52.dsc
+cd linux-source-2.6.22
+for patchfile in $CWD/patches-2.6.22/*.patch
 do
   echo "Using Patch $patchfile"
   patch -p5 < $patchfile
 done
+
 mkdir debian/binary-custom.d/zumastor debian/binary-custom.d/zumastor/patchset
 
 VERSIONSIG="CONFIG_VERSION_SIGNATURE=\"Ubuntu 2.6.24-4.6-zumastor\""
 
 for arch in i386 amd64 lpia
 do
-  cp $CWD/../config/2.6.24.2-${arch}-full \
+  cp $CWD/../config/2.6.22.18-${arch}-full \
      debian/binary-custom.d/zumastor/config.${arch}
   echo $VERSIONSIG >> debian/binary-custom.d/zumastor/config.${arch}
 done
 
 patchnum=0
-for patch in $CWD/../../ddsnap/patches/2.6.24.2/*
+for patch in $CWD/../../ddsnap/patches/2.6.22.18/*
 do
   cp $patch \
      debian/binary-custom.d/zumastor/patchset/00${patchnum}-`basename $patch`
@@ -52,7 +54,7 @@ mkdir -p $mkpatchdir/kernel-zumastor/drivers/md
 cp $CWD/../../ddsnap/kernel/dm-ddsnap.* $mkpatchdir/kernel-zumastor/drivers/md
 cd $mkpatchdir
 diff -ruNp kernel-orig kernel-zumastor > $autopatch
-cd $WORKDIR/linux-2.6.24
+cd $WORKDIR/linux-source-2.6.22
 cp $autopatch \
    debian/binary-custom.d/zumastor/patchset/00${patchnum}-AUTOdm.patch
 rm -rf $mkpatchdir
@@ -60,14 +62,10 @@ rm $autopatch
 
 echo "# Placeholder" > debian/binary-custom.d/zumastor/rules
 
-cp -r debian/abi/2.6.24-12.21 debian/abi/2.6.24-12.22
+cp -r debian/abi/2.6.22-14.46 debian/abi/2.6.22-14.52
 
-## What follows is a horrible hack. Until dch lets you force a distribution
-## it does not know about, these will unfortunately remain.
 DEBEMAIL="Zumastor Builder <zuambuild@gmail.com>" dch -v $PKGVERSION \
-         -D warty-proposed -b $CHANGELOG
-sed -i -e "s/warty-proposed/$DISTRIBUTION/" debian/changelog
-## End Ugly Hack
+         -D $DISTRIBUTION -b $CHANGELOG
 
 dpkg-buildpackage -rfakeroot -S
 echo "Results in $WORKDIR"
