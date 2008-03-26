7a36182b48b80e4f7308742fa310d0ca715cf466
williamanowak <williamanowak@9f37e8af-d821-0410-9430-6fa8b24559eb>
2008-03-26 16:33:30 +0000

    The hack was broken and didn't work in the kernel build. Fixed it so it does.

--- trunk/ddsnap/scripts/hackygenpatch.sh
+++ trunk/ddsnap/scripts/hackygenpatch.sh
@@ -3,9 +3,12 @@ basedir=`mktemp -d`
 filepath="linux/drivers/md"
 for dir in a b
 do
-  mkdir -p $basedir/$dir/$filepath
+  mkdir -p $basedir/$dir-$filepath
 done
 
-cp kernel/* $basedir/b/$filepath
-diff -ruNp $basedir/a $basedir/b
+cp kernel/* $basedir/b-$filepath
+curdir=`pwd`
+cd $basedir
+diff -ruNp a-$filepath b-$filepath
+cd $curdir
 rm -rf $basedir
