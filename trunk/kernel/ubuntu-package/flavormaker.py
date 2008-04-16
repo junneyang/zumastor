2619ae9eddf04b1f14d7184117676036dbbd9b20
williamanowak <williamanowak@9f37e8af-d821-0410-9430-6fa8b24559eb>
2008-04-16 02:20:12 +0000

    Update flavormaker tool so it works again

--- trunk/kernel/ubuntu-package/flavormaker.py
+++ trunk/kernel/ubuntu-package/flavormaker.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python
 
-APTMIRROR="http://localhost:9999/ubuntu"
-
+from optparse import OptionParser
 from debian_bundle.deb822 import Dsc
 import random
 import os
 import subprocess
-import logging
+import sys
 import logging
 
 logging.basicConfig(level=logging.DEBUG,
                         format='%(asctime)s %(levelname)s %(message)s',)
 
 class KernelBuilder:
-  def __init__(self, description_file,
-               target_distribution):
+  def __init__(self, name, description_file,
+               target_distribution, patch_directories, config_options,
+               maintainer_email, version_string, label, i386=True,
+               amd64=False, mirror='http://us.archive.ubuntu.com/ubuntu',
+               base_config='generic'):
     self._description_file = description_file
     self._description_obj = None
     self._original_source_dir = None
@@ -22,6 +24,19 @@ class KernelBuilder:
     self._builddirectory = None
     self._initialpath = os.getcwd()
     self._distribution = target_distribution
+    self._archlist = []
+    self._mirror = mirror
+    self._optslist = config_options # []
+    self._srcpatchdirs = patch_directories #[]
+    self._name = name
+    self._appendversion = version_string
+    self._basekernelconfig = base_config
+    self._label = label
+    self._email = maintainer_email
+    if i386:
+      self._archlist.append('i386')
+    if amd64:
+      self._archlist.append('amd64')
 
   def _quietRun(self, cmd):
     logging.debug('QRun: %s' % cmd)
@@ -47,12 +62,12 @@ class KernelBuilder:
 
   def _descriptionUrl(self):
     ''' Returns the URL of the dsc '''
-    return '%s/%s' % (APTMIRROR, self._description_file)
-  
+    return '%s/%s' % (self._mirror, self._description_file)
+
   def _descriptionName(self):
     file = self._descriptionUrl().split('/')[-1]
     return file
-  
+
   def _descriptionFolder(self):
     ''' Returns the URL folder the dsc is in '''
     file = self._descriptionName()
@@ -93,20 +108,18 @@ class KernelBuilder:
           logging.debug('Creating build directory..... %s' % directory)
     return self._builddirectory
 
-  def _addFlavorToBuild(self, flavorname, archlist=None):
+  def _addFlavorToBuild(self, flavorname):
     logging.info('Adding flavor to build rules')
-    if archlist is None:
-      archlist = ['i386', 'amd64']
     rulesddir = ('%s/%s/debian/rules.d' %
                  (self._buildDirectory(), self._sourceDir()))
-    for arch in archlist:
+    for arch in self._archlist:
       mkfile = '%s/%s.mk' % (rulesddir, arch)
       self._quietRun('sed -i -e "s/^custom_flavours.*/& %s/" %s' %
                      (flavorname, mkfile))
     commonvarsfile = '%s/0-common-vars.mk' % (rulesddir)
     self._quietRun('sed -i -e "s/^all_custom_flavours.*/& %s/" %s' %
                    (flavorname, commonvarsfile))
-  
+
   def _addFlavorRules(self, name, customrulesfile=None):
     logging.info('Adding flavor rules file')
     # TODO: Do something with the custom file
@@ -121,7 +134,7 @@ class KernelBuilder:
     # TODO: Do something with the custom file
     flavordir = ('%s/%s/debian/binary-custom.d/%s' %
                  (self._buildDirectory(), self._sourceDir(), name))
-    content_dict = {'arch':'i386 amd64 lpia',
+    content_dict = {'arch':' '.join(self._archlist),
                     'supported':'Generic',
                     'target':'%s kernel' % name,
                     'desc':'%s edition' % name,
@@ -135,19 +148,20 @@ class KernelBuilder:
     fh.write(content)
     fh.close()
 
-  def _addFlavorPatches(self, name, srcpatchdir):
+  def _addFlavorPatches(self, name):
     logging.info('Adding patches to flavor')
     flavordir = ('%s/%s/debian/binary-custom.d/%s' %
                  (self._buildDirectory(), self._sourceDir(), name))
-    patchsetdir = '%s/patchset' % flavordir
     patchno = 0
-    for filename in os.listdir(srcpatchdir):
-      if filename[-6:] != '.patch':
-        continue
-      original_path = '%s/%s' % (srcpatchdir, filename)
-      new_path = '%s/%03d-%s' % (patchsetdir, patchno, filename)
-      self._quietRun('cp %s %s' % (original_path, new_path))
-      patchno += 1
+    for srcpatchdir in self._srcpatchdirs:
+      patchsetdir = '%s/patchset' % flavordir
+      for filename in os.listdir(srcpatchdir):
+        if filename[-6:] != '.patch':
+          continue
+        original_path = '%s/%s' % (srcpatchdir, filename)
+        new_path = '%s/%03d-%s' % (patchsetdir, patchno, filename)
+        self._quietRun('cp %s %s' % (original_path, new_path))
+        patchno += 1
   
   def _abiFixup(self):
     logging.info('Fixing up abi bug')
@@ -158,35 +172,34 @@ class KernelBuilder:
     self._quietRun('cp -r %s/%s %s/%s' %
                    (abidir, old_abi, abidir, self._version()))
 
-  def _addFlavorCustomConfigOpts(self, name, arch, optslist):
+  def _addFlavorCustomConfigOpts(self, name, arch):
     flavordir = ('%s/%s/debian/binary-custom.d/%s' %
                  (self._buildDirectory(), self._sourceDir(), name))
     configfile = '%s/config.%s' % (flavordir, arch)
-    for opt in optslist:
+    for opt in self._optslist:
       self._quietRun('echo "%s" >> %s' % (opt, configfile))
 
-  def _addFlavor(self, name, archlist=None, basekernelconfig='generic'):
+  def _addFlavor(self, name, basekernelconfig='generic'):
+    basekernelconfig = self._basekernelconfig
     logging.info('Adding kernel flavor')
-    if archlist is None:
-      archlist = ['i386', 'amd64']
     flavordir = ('%s/%s/debian/binary-custom.d/%s' %
                  (self._buildDirectory(), self._sourceDir(), name))
     if os.path.exists(flavordir):
       raise 'Did you already create flavor %s' % name
     os.mkdir(flavordir)
     os.mkdir('%s/patchset' % flavordir)
-    for arch in archlist:
+    for arch in self._archlist:
       os.system('cat %(base)s/%(source)s/debian/config/%(arch)s/config '
                '%(base)s/%(source)s/debian/config/%(arch)s/config.%(bflav)s > '
                '%(flavdir)s/config.%(arch)s' %
                 {'base':self._buildDirectory(),
                  'source':self._sourceDir(), 'bflav':basekernelconfig,
                  'arch':arch, 'flavdir':flavordir})
-      self._addFlavorCustomConfigOpts(name, arch, ['CONFIG_DM_DDSNAP=m'])
-    self._addFlavorToBuild(name, archlist)
+      self._addFlavorCustomConfigOpts(name, arch)
+    self._addFlavorToBuild(name)
     self._addFlavorRules(name)
     self._addFlavorVars(name)
-    self._addFlavorPatches(name, '/home/wan/zumastor/ddsnap/patches/2.6.24.2/')
+    self._addFlavorPatches(name)
 
   def _updateChangeLog(self, maintainer, newversion, entry):
     logging.info('Updating changelog')
@@ -197,19 +210,60 @@ class KernelBuilder:
     sp = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE)
     sp.stdin.write('\n')
     sp.wait()
-                                                       
+
   def main(self):
     self._getFiles()
-    self._addFlavor('zumastor')
+    self._addFlavor(self._name)
     self._abiFixup()
-    self._updateChangeLog('Zumastor Builder <zuambuild@gmail.com>',
-                          '%s~zumappa1' % (self._version()),
-                          'Upstream Package with Zumastor.org patches')
+    self._updateChangeLog(self._email, '%s~%s' % (self._version(), self._appendversion),
+                          self._label)
 
 if __name__ == '__main__':
-  hardy_2624 = KernelBuilder('pool/main/l/linux/linux_2.6.24-12.22.dsc',
-                             'hardy')
-  hardy_2624.main()
-  
-  gutsy_2622 = KernelBuilder('pool/main/l/linux-source-2.6.22/linux-source-2.6.22_2.6.22-14.52.dsc', 'gutsy')
-  gutsy_2622.main()
+  parser = OptionParser()
+  parser.add_option('-m', '--mirror',
+                    default='http://us.archive.ubuntu.com/ubuntu',
+                    help='Ubuntu Archive Mirror')
+  parser.add_option('-r', '--release',
+                    help='Release to build for (gutsy, hardy, etc)')
+  parser.add_option('-d', '--dscfile',
+                    help='path to dsc file eg. pool/main/l/linux/linux.dsc')
+  parser.add_option('-p', '--patches', action='append', default=[],
+                    help='Patch directory to insert into flavor')
+  parser.add_option('-c', '--config', help='Append line to kernel config',
+                    action='append', default=[])
+  parser.add_option('-e', '--email',
+                    default='Flavor Maker <flavormaker@unconfigured>',
+                    help='Changelog style email \'User Bob <user@bob.com>\'',)
+  parser.add_option('-v', '--version', default='flavormaker1',
+                    help='Version to append with ~ to package name. eg: myppa1',)
+  parser.add_option('-l', '--label', default='flavormaker modified package',
+                    help='Label for this modified kernel. eg: Upstream with my'
+                    ' patches',)
+  parser.add_option('-i', '--i386', help='Build for i386',
+                    action='store_true', default=False)
+  parser.add_option('-a', '--amd64', help='Build for amd64',
+                    action='store_true', default=False)
+  parser.add_option('-n', '--name', help='Flavor Name')
+  parser.add_option('-b', '--baseconfig',
+                    help='Base Kernel Configuration (generic is default)',
+                    default='generic')
+  (options, args) = parser.parse_args()
+  if options.release is None:
+    print 'ERROR: You must provide a release to build for'
+    parser.print_help()
+    sys.exit(1)
+  if options.dscfile is None:
+    print 'ERROR: You must provide a kernel dsc path to build from'
+    parser.print_help()
+    sys.exit(1)
+  if (not options.i386) and (not options.amd64):
+    print options
+    print 'ERROR: You must pick at least one arch to build for (-a or -i)'
+    parser.print_help()
+    sys.exit(1)
+
+  flavor = KernelBuilder(options.name, options.dscfile, options.release,
+                         options.patches, options.config, options.email,
+                         options.version, options.label, options.i386,
+                         options.amd64, options.mirror, options.baseconfig)
+  flavor.main()
