c46d27809d678d205b7db33dbecb442a5e8f0f12
williamanowak <williamanowak@9f37e8af-d821-0410-9430-6fa8b24559eb>
2008-04-22 21:49:56 +0000

    - Update the CBTB harness to support the new CBTB/test interface - runtests.sh gets new ENV Variables - test-zuma-dapper-i386 gets logic to read DEV[0-9]SIZE from the test

--- cbtb/host-scripts/test-zuma-dapper-i386.sh
+++ cbtb/host-scripts/test-zuma-dapper-i386.sh
@@ -92,18 +92,30 @@ largest_hddsize=0
 for f in ${execfiles}
 do
   hdbsize=`awk -F = '/^HDBSIZE=[0-9]+$/ { print $2; }' ./${f} | tail -1`
+  if [ "x$hdbsize" == "x" ]
+  then
+    hdbsize=`awk -F = '/^DEV1SIZE=[0-9]+$/ { print $2; }' ./${f} | tail -1`
+  fi
   if [ "x$hdbsize" != "x" ] ; then
     if [ "$hdbsize" -ge "$largest_hdbsize" ] ; then
       largest_hdbsize=$hdbsize
     fi
   fi
   hdcsize=`awk -F = '/^HDCSIZE=[0-9]+$/ { print $2; }' ./${f} | tail -1`
+  if [ "x$hdcsize" == "x" ]
+  then
+    hdcsize=`awk -F = '/^DEV2SIZE=[0-9]+$/ { print $2; }' ./${f} | tail -1`
+  fi
   if [ "x$hdcsize" != "x" ] ; then
     if [ "$hdcsize" -ge "$largest_hdcsize" ] ; then
       largest_hdcsize=$hdcsize
     fi
   fi
   hddsize=`awk -F = '/^HDDSIZE=[0-9]+$/ { print $2; }' ./${f} | tail -1`
+  if [ "x$hddsize" == "x" ]
+  then
+    hddsize=`awk -F = '/^DEV3SIZE=[0-9]+$/ { print $2; }' ./${f} | tail -1`
+  fi
   if [ "x$hddsize" != "x" ] ; then
     if [ "$hddsize" -ge "$largest_hddsize" ] ; then
       largest_hddsize=$hddsize
