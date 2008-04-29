0d3133ee61a2c0a0510da90160fcc889e60f0fdb
williamanowak <williamanowak@9f37e8af-d821-0410-9430-6fa8b24559eb>
2008-04-29 21:01:19 +0000

    This change will unify version output for ddsnap and zumastor ddsnap 0.8.0-r1609m built on Tue Apr 29 13:56:49 PDT 2008 by willn@lackey and zumastor 0.8.0-r1609m built on Tue Apr 29 13:56:49 PDT 2008 by willn@lackey - Update debian rules clean action for zumastor - Update /etc/zumastor/version target in zumastor Makefile - change ddsnap from VERSION to VERSION_STRING version information - update buildcurrent.sh to generate a VERSION_STRING file and use it

--- buildcurrent.sh
+++ buildcurrent.sh
@@ -53,15 +53,15 @@ if [ "x$VERSION" = "x" ] ; then
   exit 1
 fi
 
-# Get the svn revision number from the file SVNREV, svnversion, or by scraping
+# Get the svn revision number from the file REVISION, svnversion, or by scraping
 # the output of svn log, in order until one is successful
-SVNREV=`awk '/^[0-9]+$/ { print $1; }' SVNREV || svnversion | tr [A-Z] [a-z] || svn info zumastor | grep ^Revision:  | cut -d\  -f2`
-if [ "x$SVNREV" = "x" ]
+REVISION=`awk '/^[0-9]+$/ { print $1; }' REVISION || svnversion | tr [A-Z] [a-z] || svn info zumastor | grep ^Revision:  | cut -d\  -f2`
+if [ "x$REVISION" = "x" ]
 then
-  SVNREV=unknown0
-elif [ "x$SVNREV" = "xexported" ]
+  REVISION=unknown0
+elif [ "x$REVISION" = "xexported" ]
 then
-  SVNREV=exported0
+  REVISION=exported0
 fi
 SRC=${PWD}
 BUILD_DIR=${SRC}/build
@@ -77,36 +77,32 @@ HOSTARCH=$ARCH
 
 
 [ -d $BUILD_DIR ] || mkdir $BUILD_DIR
-[ -d $BUILD_DIR/r${SVNREV} ] || mkdir $BUILD_DIR/r${SVNREV}
+[ -d $BUILD_DIR/r${REVISION} ] || mkdir $BUILD_DIR/r${REVISION}
 cp $kconfig $BUILD_DIR/$KERNEL_VERSION.config
 pushd $BUILD_DIR >> $LOG || exit 1
 
-
-
-
+VERSION_STRING="${VERSION}-r${REVISION}"
 
 echo -n Building zumastor Debian package...
 pushd ${SRC}/zumastor >> $LOG || exit 1
 
-echo ${SVNREV} >SVNREV
-echo ${VERSION} >SVNVERSION
-
+echo ${VERSION_STRING} > VERSION_STRING
 [ -f debian/changelog ] && rm debian/changelog
-EDITOR=/bin/true VISUAL=/bin/true dch --create --package zumastor -u low --no-query -v $VERSION-r$SVNREV "revision $SVNREV" || exit 1
+EDITOR=/bin/true VISUAL=/bin/true dch --create --package zumastor -u low --no-query -v $VERSION_STRING "revision $REVISION" || exit 1
 dpkg-buildpackage -I.svn -uc -us -rfakeroot >> $LOG || exit 1
 popd >> $LOG
-mv ${SRC}/*.changes ${SRC}/*.deb ${SRC}/*.tar.gz ${SRC}/*.dsc ${BUILD_DIR}/r${SVNREV}
+mv ${SRC}/*.changes ${SRC}/*.deb ${SRC}/*.tar.gz ${SRC}/*.dsc ${BUILD_DIR}/r${REVISION}
 echo -e "done.\n"
 
 echo -n Building ddsnap Debian package...
 pushd ${SRC}/ddsnap >> $LOG || exit 1
-echo ${SVNREV} >SVNREV
+echo ${VERSION_STRING} > VERSION_STRING
 [ -f debian/changelog ] && rm debian/changelog
-EDITOR=/bin/true VISUAL=/bin/true dch --create --package ddsnap -u low --no-query -v $VERSION-r$SVNREV "revision $SVNREV" || exit 1
+EDITOR=/bin/true VISUAL=/bin/true dch --create --package ddsnap -u low --no-query -v $VERSION_STRING "revision $REVISION" || exit 1
 dpkg-buildpackage -I.svn -uc -us -rfakeroot >> $LOG || exit 1
 make genpatches
 popd >> $LOG
-mv ${SRC}/*.changes ${SRC}/*.deb ${SRC}/*.tar.gz ${SRC}/*.dsc ${BUILD_DIR}/r${SVNREV}
+mv ${SRC}/*.changes ${SRC}/*.deb ${SRC}/*.tar.gz ${SRC}/*.dsc ${BUILD_DIR}/r${REVISION}
 echo -e "done.\n"
 
 if [ -e linux-${KERNEL_VERSION} ] ; then
@@ -151,16 +147,16 @@ then
      SUBARCH=i386
    fi
    make -j4 ARCH=um SUBARCH=$SUBARCH linux
-   [ -d ../r${SVNREV} ] || mkdir ../r${SVNREV}
-   mv linux ../r${SVNREV}/linux-${HOSTARCH}-r${SVNREV}
+   [ -d ../r${REVISION} ] || mkdir ../r${REVISION}
+   mv linux ../r${REVISION}/linux-${HOSTARCH}-r${REVISION}
   else
     echo -n Building kernel package...
-    fakeroot make-kpkg --append_to_version=-zumastor-r$SVNREV --revision=1.0 --initrd  --mkimage="mkinitramfs -o /boot/initrd.img-%s %s" --bzimage kernel_image kernel_headers >> $LOG </dev/null || exit 1
+    fakeroot make-kpkg --append_to_version=-zumastor-r$REVISION --revision=1.0 --initrd  --mkimage="mkinitramfs -o /boot/initrd.img-%s %s" --bzimage kernel_image kernel_headers >> $LOG </dev/null || exit 1
     for kfile in \
-      kernel-image-${KERNEL_VERSION}-zumastor-r${SVNREV}_1.0_${HOSTARCH}.deb \
-      kernel-headers-${KERNEL_VERSION}-zumastor-r${SVNREV}_1.0_${HOSTARCH}.deb
+      kernel-image-${KERNEL_VERSION}-zumastor-r${REVISION}_1.0_${HOSTARCH}.deb \
+      kernel-headers-${KERNEL_VERSION}-zumastor-r${REVISION}_1.0_${HOSTARCH}.deb
     do
-      [ -f ../$kfile ] && mv ../$kfile ${BUILD_DIR}/r${SVNREV}/
+      [ -f ../$kfile ] && mv ../$kfile ${BUILD_DIR}/r${REVISION}/
     done
   fi
   popd >> $LOG
