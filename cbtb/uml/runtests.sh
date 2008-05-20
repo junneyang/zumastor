7987901547e901bd05af273d3ae7bc54ce40e57c
williamanowak <williamanowak@9f37e8af-d821-0410-9430-6fa8b24559eb>
2008-05-20 18:38:25 +0000

    Added functionality to skip EXPECT_FAIL tests eg. `./smoke-test.sh --all --no-fail` will skip tests with EXPECT_FAIL=1

--- cbtb/uml/runtests.sh
+++ cbtb/uml/runtests.sh
@@ -13,9 +13,11 @@ set -e
 
 smoketests1="snapshot-zumastor-xfs-2045G.sh"
 smoketests2="replication-zumastor.sh"
-  
+nofail=0
+all=0
+
 if [ "x$ARCH" = "x" ] ; then
-  ARCH=`dpkg --print-arch || echo i386`
+  ARCH=`dpkg --print-architecture || echo i386`
 fi
 
 if [ "x$DIST" = "x" ] ; then
@@ -24,10 +26,11 @@ fi
 
 usage() {
   cat <<EOF
-$0 [--all]
+$0 [--all] [--no-fail]
 
 --all   Run all tests in cbtb/tests/1/ and cbtb/tests/2/, rather than just
         the smoketests: $smoketests1 $smoketests2.
+--no-fail  Do not try and run failing tests
 
 Environment variables:
 ARCH - the CPU/ABI architecture to test.     currently: $ARCH
@@ -37,27 +40,65 @@ EOF
 
 testparent=../tests
 
-tests1=$smoketests1
-tests2=$smoketests2
+while true
+do
+  case $1 in
+    --all)
+      all=1
+      shift
+      ;;
 
-case $1 in
---all)
+   --no-fail)
+      nofail=1
+      shift
+      ;;
+
+   --help|-h)
+     usage
+     exit 1
+     ;;
+
+   --)
+     shift
+     break
+     ;;
+
+   *)
+     echo "$1 is unknown"
+     #echo "Internal error"
+     break
+     ;;
+   esac
+done
+
+summary=`mktemp`
+
+if [ $all -eq 1 ]
+then
+  echo "INFO: Running all tests"
   tests1=`cd $testparent/1 && echo *.sh`
   tests2=`cd $testparent/2 && echo *.sh`
   tests3=`cd $testparent/3 && echo *.sh`
-  shift
-  ;;
+else
+  tests1=$smoketests1
+  tests2=$smoketests2
+fi
 
---help|-h)
-  usage
-  exit 1
-  ;;
-esac
-  
-summary=`mktemp`
+[ $nofail -eq 1 ] && echo "INFO: Skipping EXPECT_FAIL tests"
+
+skip_test() {
+  testname=$1
+  egrep -q EXPECT_FAIL=1 $testname && [ $nofail -eq 1 ]
+  return $?
+}
 
 for test in $tests1
 do
+  if skip_test $testparent/1/$test
+  then
+    echo SKIP $test >> $summary
+    continue
+  fi
   if DIST=$DIST ARCH=$ARCH time tunbr ./test-zuma-uml.sh $testparent/1/$test
   then
     echo PASS $test >>$summary
@@ -68,6 +109,11 @@ done
 
 for test in $tests2
 do
+  if skip_test $testparent/2/$test
+  then
+    echo SKIP $test >> $summary
+    continue
+  fi
   if  DIST=$DIST ARCH=$ARCH time tunbr tunbr ./test-zuma-uml.sh $testparent/2/$test
   then
     echo PASS $test >>$summary
@@ -78,6 +124,11 @@ done
 
 for test in $tests3
 do
+  if skip_test $testparent/3/$test
+  then
+    echo SKIP $test >> $summary
+    continue
+  fi
   if  DIST=$DIST ARCH=$ARCH time tunbr tunbr tunbr ./test-zuma-uml.sh $testparent/3/$test
   then
     echo PASS $test >>$summary
